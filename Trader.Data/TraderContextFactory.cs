1cff6f1d2f2ffdfc81e81c3354204bf04b7cfc63
jorge <jorge.candeias@protonmail.com>
2021-03-20 20:42:51 +0000

    Added context factory

---
+++ Trader.Data/TraderContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Trader.Data
+{
+    internal class TraderContextFactory : IDesignTimeDbContextFactory<TraderContext>
+    {
+        public TraderContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<TraderContext>();
+
+            builder.UseSqlite("Data Source=:memory:");
+
+            return new TraderContext(builder.Options);
+        }
+    }
+}

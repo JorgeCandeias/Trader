9a943a85aada7a7af235f192f2a49c4430e0c7b4
jorge <jorge.candeias@protonmail.com>
2021-03-21 03:17:37 +0000

    Added migration

--- Trader.Data/Migrations/20210321031529_TradeEntity.Designer.cs
+++ Trader.Data/Migrations/20210321031529_TradeEntity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trader.Data;
 
 namespace Trader.Data.Migrations
 {
     [DbContext(typeof(TraderContext))]
-    partial class TraderContextModelSnapshot : ModelSnapshot
+    [Migration("20210321031529_TradeEntity")]
+    partial class TradeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Trader.Data/Migrations/20210321031529_TradeEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trader.Data.Migrations
+{
+    public partial class TradeEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TradeEntity",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Symbol = table.Column<string>(type: "TEXT", nullable: false),
+                    OrderId = table.Column<long>(type: "INTEGER", nullable: false),
+                    OrderListId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Quantity = table.Column<decimal>(type: "TEXT", nullable: false),
+                    QuoteQuantity = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Commission = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CommissionAsset = table.Column<string>(type: "TEXT", nullable: false),
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsBuyer = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsMaker = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsBestMatch = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TradeEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TradeEntity_Symbol_Id",
+                table: "TradeEntity",
+                columns: new[] { "Symbol", "Id" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TradeEntity_Symbol_OrderId",
+                table: "TradeEntity",
+                columns: new[] { "Symbol", "OrderId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TradeEntity");
+        }
+    }
+}

7bbd2f4b261e287f7cd199d070881e12f575d457
jorge <jorge.candeias@protonmail.com>
2021-03-21 03:14:59 +0000

    Added TradeEntity

--- Trader.Data/TraderContext.cs
+++ Trader.Data/TraderContext.cs
@@ -17,6 +17,10 @@ namespace Trader.Data
 
             modelBuilder.Entity<OrderEntity>().HasKey(x => x.OrderId);
             modelBuilder.Entity<OrderEntity>().HasIndex(x => new { x.Symbol, x.OrderId });
+
+            modelBuilder.Entity<TradeEntity>().HasKey(x => x.Id);
+            modelBuilder.Entity<TradeEntity>().HasIndex(x => new { x.Symbol, x.Id });
+            modelBuilder.Entity<TradeEntity>().HasIndex(x => new { x.Symbol, x.OrderId });
         }
     }
 
@@ -39,4 +43,19 @@ namespace Trader.Data
         DateTime UpdateTime,
         bool IsWorking,
         decimal OriginalQuoteOrderQuantity);
+
+    internal record TradeEntity(
+        string Symbol,
+        long Id,
+        long OrderId,
+        long OrderListId,
+        decimal Price,
+        decimal Quantity,
+        decimal QuoteQuantity,
+        decimal Commission,
+        string CommissionAsset,
+        DateTime Time,
+        bool IsBuyer,
+        bool IsMaker,
+        bool IsBestMatch);
 }

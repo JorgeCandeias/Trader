ba8657a671580bec92061ed2704d71b744554b69
jorge <jorge.candeias@protonmail.com>
2021-04-14 13:40:18 +0100

    Added statistics

--- Trader.Trading.Abstractions/Algorithms/ISignificantOrderResolver.cs
+++ Trader.Trading.Abstractions/Algorithms/ISignificantOrderResolver.cs
@@ -65,16 +65,19 @@ namespace Trader.Trading.Algorithms
         }
     }
 
-    public record Statistics(decimal AvgPerHourDay1, decimal AvgPerHourDay7, decimal AvgPerHourDay30)
+    public record Statistics(decimal AvgPerHourDay1, decimal AvgPerHourDay7, decimal AvgPerHourDay30, decimal AvgPerDay1, decimal AvgPerDay7, decimal AvgPerDay30)
     {
-        public static Statistics Zero { get; } = new Statistics(0, 0, 0);
+        public static Statistics Zero { get; } = new Statistics(0, 0, 0, 0, 0, 0);
 
         public static Statistics FromProfit(Profit profit)
         {
             return new Statistics(
                 profit.D1 / 24m,
                 profit.D7 / (24m * 7m),
-                profit.D30 / (24m * 30m));
+                profit.D30 / (24m * 30m),
+                profit.D1,
+                profit.D7 / 7m,
+                profit.D30 / 30m);
         }
     }
 }
--- Trader.Trading/TradingHost.cs
+++ Trader.Trading/TradingHost.cs
@@ -74,8 +74,8 @@ namespace Trader.Trading
                 var stats = await algo.GetStatisticsAsync(_cancellation.Token);
 
                 _logger.LogInformation(
-                    "{Name} reports {Symbol,7} profit as (T: {@Today,6:N2}, T-1: {@Yesterday,6:N2}, W: {@ThisWeek,6:N2}, W-1: {@PrevWeek,6:N2}, M: {@ThisMonth,6:N2}, Y: {@ThisYear,6:N2}) (APH1: {@AveragePerHourDay1,5:N2}, APH7: {@AveragePerHourDay7,5:N2}, APH30: {@AveragePerHourDay30,5:N2})",
-                    Name, algo.Symbol, profit.Today, profit.Yesterday, profit.ThisWeek, profit.PrevWeek, profit.ThisMonth, profit.ThisYear, stats.AvgPerHourDay1, stats.AvgPerHourDay7, stats.AvgPerHourDay30);
+                    "{Name} reports {Symbol,7} profit as (T: {@Today,6:N2}, T-1: {@Yesterday,6:N2}, W: {@ThisWeek,6:N2}, W-1: {@PrevWeek,6:N2}, M: {@ThisMonth,6:N2}, Y: {@ThisYear,6:N2}) (APH1: {@AveragePerHourDay1,6:N2}, APH7: {@AveragePerHourDay7,6:N2}, APH30: {@AveragePerHourDay30,6:N2}, APD1: {@AveragePerDay1,6:N2}, APD7: {@AveragePerDay7,6:N2}, APD30: {@AveragePerDay30,6:N2})",
+                    Name, algo.Symbol, profit.Today, profit.Yesterday, profit.ThisWeek, profit.PrevWeek, profit.ThisMonth, profit.ThisYear, stats.AvgPerHourDay1, stats.AvgPerHourDay7, stats.AvgPerHourDay30, stats.AvgPerDay1, stats.AvgPerDay7, stats.AvgPerDay30);
 
                 profits.Add(profit);
             }
@@ -84,7 +84,7 @@ namespace Trader.Trading
             var totalStats = Statistics.FromProfit(totalProfit);
 
             _logger.LogInformation(
-                "{Name} reports   total profit as (T: {@Today,6:N2}, T-1: {@Yesterday,6:N2}, W: {@ThisWeek,6:N2}, W-1: {@PrevWeek,6:N2}, M: {@ThisMonth,6:N2}, Y: {@ThisYear,6:N2}) (APH1: {@AveragePerHourDay1,5:N2}, APH7: {@AveragePerHourDay7,5:N2}, APH30: {@AveragePerHourDay30,5:N2})",
+                "{Name} reports   total profit as (T: {@Today,6:N2}, T-1: {@Yesterday,6:N2}, W: {@ThisWeek,6:N2}, W-1: {@PrevWeek,6:N2}, M: {@ThisMonth,6:N2}, Y: {@ThisYear,6:N2}) (APH1: {@AveragePerHourDay1,6:N2}, APH7: {@AveragePerHourDay7,6:N2}, APH30: {@AveragePerHourDay30,6:N2}, APD1: {@AveragePerDay1,6:N2}, APD7: {@AveragePerDay7,6:N2}, APD30: {@AveragePerDay30,6:N2})",
                 Name,
                 totalProfit.Today,
                 totalProfit.Yesterday,
@@ -94,7 +94,10 @@ namespace Trader.Trading
                 totalProfit.ThisYear,
                 totalStats.AvgPerHourDay1,
                 totalStats.AvgPerHourDay7,
-                totalStats.AvgPerHourDay30);
+                totalStats.AvgPerHourDay30,
+                totalStats.AvgPerDay1,
+                totalStats.AvgPerDay7,
+                totalStats.AvgPerDay30);
         }
     }
 }

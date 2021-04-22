8b963146d168dfa3812d76268a28b083e8ba376c
jorge <jorge.candeias@protonmail.com>
2021-04-22 17:55:05 +0100

    Refactored timer

--- Trader.Core/Timers/SafeTimer.cs
+++ Trader.Core/Timers/SafeTimer.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,10 +23,11 @@ namespace Trader.Core.Timers
             _logger = logger;
         }
 
-        private readonly CancellationTokenSource _cancellation = new();
-
+        private static string Name => nameof(SafeTimer);
+        private CancellationTokenSource? _cancellation;
         private Timer? _timer;
         private Task _task = Task.CompletedTask;
+        private readonly object _lock = new();
 
         [SuppressMessage("Major Bug", "S3168:\"async\" methods should not return \"void\"", Justification = "Timer Event Handler")]
         private async void Handler(object? _)
@@ -36,7 +36,7 @@ namespace Trader.Core.Timers
             try
             {
                 using var timeoutCancellation = new CancellationTokenSource(_timeout);
-                using var combinedCancellation = CancellationTokenSource.CreateLinkedTokenSource(_cancellation.Token, timeoutCancellation.Token);
+                using var combinedCancellation = CancellationTokenSource.CreateLinkedTokenSource(_cancellation?.Token ?? default, timeoutCancellation.Token);
 
                 _task = _callback(combinedCancellation.Token);
 
@@ -44,29 +44,40 @@ namespace Trader.Core.Timers
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "{ServiceName} caught exception {Message}", nameof(SafeTimer), ex.Message);
-
-                if (Debugger.IsAttached)
-                {
-                    throw;
-                }
+                _logger.LogError(ex, "{ServiceName} caught exception {Message}", Name, ex.Message);
             }
 
             // schedule the next tick
-            try
+            lock (_lock)
             {
-                _timer?.Change(_period, Timeout.InfiniteTimeSpan);
-            }
-            catch (ObjectDisposedException)
-            {
-                // noop
+                try
+                {
+                    _timer?.Change(_period, Timeout.InfiniteTimeSpan);
+                }
+                catch (ObjectDisposedException)
+                {
+                    // noop
+                }
             }
         }
 
         public Task StartAsync(CancellationToken cancellationToken = default)
         {
-            if (_timer is null)
+            EnsureNotDisposed();
+
+            // quick path for an already running timer
+            if (_timer is not null) return Task.CompletedTask;
+
+            // thread-safe path for starting the timer
+            lock (_lock)
             {
+                // thread-safe path for an already running timer
+                if (_timer is not null) return Task.CompletedTask;
+
+                // renew the cancellation
+                _cancellation = new();
+
+                // renew the timer
                 _timer = new Timer(Handler, null, _dueTime, Timeout.InfiniteTimeSpan);
             }
 
@@ -75,31 +86,80 @@ namespace Trader.Core.Timers
 
         public async Task StopAsync(CancellationToken cancellationToken = default)
         {
-            if (_timer is not null)
+            EnsureNotDisposed();
+
+            _logger.LogInformation("{ServiceName} stopping...", Name);
+
+            // quick path for an already stopped timer
+            if (_timer is null) return;
+
+            // thread-safe path for stopping the timer
+            Task task;
+            lock (_lock)
             {
-                _cancellation.Cancel();
+                // thread-safe check for the stopped timer
+                if (_timer is null) return;
+
+                // release the cancellation - this will also raise the token
+                _cancellation?.Dispose();
+                _cancellation = null;
+
+                // release the dotnet timer to stop ticking
                 _timer.Dispose();
                 _timer = null;
 
-                var task = Interlocked.Exchange(ref _task, Task.CompletedTask);
-                try
-                {
-                    await task;
-                }
-                catch (OperationCanceledException)
-                {
-                    // noop
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "{ServiceName} caught exception {Message}", nameof(SafeTimer), ex.Message);
-                }
+                // keep the last task so we can await for it outside the lock
+                task = _task;
+
+                // release the last task for garbage collection after we are done with it
+                _task = Task.CompletedTask;
             }
+
+            // await for the last task now
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{ServiceName} caught exception {Message}", Name, ex.Message);
+            }
+        }
+
+        #region Disposable
+
+        private bool _disposed = false;
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _cancellation?.Dispose();
+                _timer?.Dispose();
+            }
+
+            _disposed = true;
         }
 
         public void Dispose()
         {
-            _timer?.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
+
+        ~SafeTimer()
+        {
+            Dispose(false);
+        }
+
+        private void EnsureNotDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(Name);
+            }
+        }
+
+        #endregion Disposable
     }
 }

f832c2510113e884277d5ebf2207c6fff84fd0e3
jorge <jorge.candeias@protonmail.com>
2021-04-23 20:31:49 +0100

    Added sql tables

---
+++ Trader.Data.Sql.Database/Tables/Order.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[Symbol] NVARCHAR(100) NOT NULL,
+    [OrderId] BIGINT NOT NULL,
+    [OrderListId] BIGINT NOT NULL,
+    [ClientOrderId] NVARCHAR(100) NOT NULL,
+    [Price] DECIMAL (18,8) NOT NULL,
+    [OriginalQuantity] DECIMAL (18,8) NOT NULL,
+    [ExecutedQuantity] DECIMAL (18,8) NOT NULL,
+    [CummulativeQuoteQuantity] DECIMAL (18,8) NOT NULL,
+    [OriginalQuoteOrderQuantity] DECIMAL (18,8) NOT NULL,
+    [Status] INT NOT NULL,
+    [TimeInForce] INT NOT NULL,
+    [Type] INT NOT NULL,
+    [Side] INT NOT NULL,
+    [StopPrice] DECIMAL (18,8) NOT NULL,
+    [IcebergQuantity] DECIMAL (18,8) NOT NULL,
+    [Time] DATETIME2(7) NOT NULL,
+    [UpdateTime] DATETIME2(7) NOT NULL,
+    [IsWorking] BIT NOT NULL,
+
+    CONSTRAINT [PK_Order] PRIMARY KEY CLUSTERED
+    (
+        [Symbol],
+        [OrderId]
+    )
+)
---
+++ Trader.Data.Sql/Models.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Trader.Data.Sql
+{
+    internal record OrderEntity(
+        string Symbol,
+        long OrderId,
+        long OrderListId,
+        string ClientOrderId,
+        decimal Price,
+        decimal OriginalQuantity,
+        decimal ExecutedQuantity,
+        decimal CummulativeQuoteQuantity,
+        decimal OriginalQuoteOrderQuantity,
+        int Status,
+        int TimeInForce,
+        int Type,
+        int Side,
+        decimal StopPrice,
+        decimal IcebergQuantity,
+        DateTime Time,
+        DateTime UpdateTime,
+        bool IsWorking);
+
+    internal record TradeEntity(
+        string Symbol,
+        long Id,
+        long OrderId,
+        long OrderListId,
+        decimal Price,
+        decimal Quantity,
+        decimal QuoteQuantity,
+        decimal Commission,
+        string CommissionAsset,
+        DateTime Time,
+        bool IsBuyer,
+        bool IsMaker,
+        bool IsBestMatch);
+}

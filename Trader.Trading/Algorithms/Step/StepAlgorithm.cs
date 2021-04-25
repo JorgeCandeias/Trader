0217c4c467e137a1c1638577303c1bc813d787d8
jorge <jorge.candeias@protonmail.com>
2021-04-25 17:22:17 +0100

    Added note

--- Trader.Trading/Algorithms/Step/StepAlgorithm.cs
+++ Trader.Trading/Algorithms/Step/StepAlgorithm.cs
@@ -531,6 +531,7 @@ namespace Trader.Trading.Algorithms.Step
             }
 
             // apply the non-significant open buy orders to the bands
+            // todo: this looks like a bug - we can also have significant transient orders (live partial buys) that are not part of the significant set above
             var orders = await _repository.GetNonSignificantTransientOrdersBySideAsync(_options.Symbol, OrderSide.Buy, cancellationToken);
             foreach (var order in orders)
             {

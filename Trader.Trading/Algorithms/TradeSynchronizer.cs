a2c7d09a847ea6809b68c04d68f1ee02907128d3
jorge <jorge.candeias@protonmail.com>
2021-04-26 18:41:56 +0100

    Cleanup

--- Trader.Trading/Algorithms/TradeSynchronizer.cs
+++ Trader.Trading/Algorithms/TradeSynchronizer.cs
@@ -29,20 +29,26 @@ namespace Trader.Trading.Algorithms
             var watch = Stopwatch.StartNew();
 
             // get the last trade id available
-            var tradeId = await _repository.GetMaxTradeIdAsync(symbol, cancellationToken);
+            var tradeId = await _repository
+                .GetMaxTradeIdAsync(symbol, cancellationToken)
+                .ConfigureAwait(false);
 
             // pull all trades
             var count = 0;
             while (!cancellationToken.IsCancellationRequested)
             {
                 // query for the next trades
-                var trades = await _trader.GetAccountTradesAsync(new GetAccountTrades(symbol, null, null, tradeId + 1, 1000, null, _clock.UtcNow), cancellationToken);
+                var trades = await _trader
+                    .GetAccountTradesAsync(new GetAccountTrades(symbol, null, null, tradeId + 1, 1000, null, _clock.UtcNow), cancellationToken)
+                    .ConfigureAwait(false);
 
                 // break if we got all trades
                 if (trades.Count is 0) break;
 
                 // persist all new trades in this page
-                await _repository.SetTradesAsync(trades, cancellationToken);
+                await _repository
+                    .SetTradesAsync(trades, cancellationToken)
+                    .ConfigureAwait(false);
 
                 // keep the last trade
                 tradeId = trades.Max!.Id;

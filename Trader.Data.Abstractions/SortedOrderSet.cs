2ed87b3d758a2327054a9864179ef5267d02bc7e
jorge <jorge.candeias@protonmail.com>
2021-04-27 14:56:13 +0100

    Refactored

--- Trader.Data.Abstractions/SortedOrderSet.cs
+++ Trader.Data.Abstractions/SortedOrderSet.cs
@@ -1,31 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Trader.Models;
+using Trader.Models.Comparers;
 
 namespace Trader.Data
 {
     // todo: make this class immutable
     public class SortedOrderSet : SortedSet<OrderQueryResult>
     {
-        public SortedOrderSet() : base(OrderIdComparer.Instance)
+        public SortedOrderSet() : base(OrderQueryResultOrderIdComparer.Instance)
         {
-        }
-
-        private class OrderIdComparer : IComparer<OrderQueryResult>
-        {
-            private OrderIdComparer()
-            {
-            }
-
-            public int Compare(OrderQueryResult? x, OrderQueryResult? y)
-            {
-                if (x is null) throw new ArgumentNullException(nameof(x));
-                if (y is null) throw new ArgumentNullException(nameof(y));
-
-                return x.OrderId.CompareTo(y.OrderId);
-            }
-
-            public static OrderIdComparer Instance { get; } = new OrderIdComparer();
         }
     }
 }
---
+++ Trader.Models/Comparers/OrderQueryResultOrderIdComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trader.Models.Comparers
+{
+    public class OrderQueryResultOrderIdComparer : IComparer<OrderQueryResult>
+    {
+        private OrderQueryResultOrderIdComparer()
+        {
+        }
+
+        public int Compare(OrderQueryResult? x, OrderQueryResult? y)
+        {
+            if (x is null) throw new ArgumentNullException(nameof(x));
+            if (y is null) throw new ArgumentNullException(nameof(y));
+
+            var bySymbol = Comparer<string>.Default.Compare(x.Symbol, y.Symbol);
+            if (bySymbol is not 0) return bySymbol;
+
+            return Comparer<long>.Default.Compare(x.OrderId, y.OrderId);
+        }
+
+        public static OrderQueryResultOrderIdComparer Instance { get; } = new OrderQueryResultOrderIdComparer();
+    }
+}

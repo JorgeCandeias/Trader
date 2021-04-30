0d3192e81c92e3f6df3a5c7961e1f3c1adf6cadd
jorge <jorge.candeias@protonmail.com>
2021-04-30 13:51:28 +0100

    Cleanup

--- Trader.Trading/Algorithms/Step/StepAlgorithmOptions.cs
+++ Trader.Trading/Algorithms/Step/StepAlgorithmOptions.cs
@@ -27,12 +27,6 @@ namespace Trader.Trading.Algorithms.Step
         [Range(0.001, 1)]
         public decimal TargetQuoteBalanceFractionPerBand { get; set; } = 0.005m;
 
-        /*
-        [Required]
-        [Range(0, 100)]
-        public decimal MinQuoteAssetQuantityPerOrder { get; set; } = 0m;
-        */
-
         [Required]
         [Range(1, 10)]
         public decimal ExtraAmountPerBandMultiplier { get; set; } = 1.00m;

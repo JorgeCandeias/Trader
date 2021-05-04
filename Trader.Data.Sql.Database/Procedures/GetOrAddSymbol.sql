f7068121175a9848b11863ba73042802d6185c65
jorge <jorge.candeias@protonmail.com>
2021-05-04 19:45:56 +0100

    Added Symbol table

---
+++ Trader.Data.Sql.Database/Procedures/GetOrAddSymbol.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[GetOrAddSymbol]
+	@Name NVARCHAR(100),
+	@Id INT OUTPUT
+AS
+
+/* quick path for existing symbol */
+SELECT @Id = [Id] FROM [Symbol] WHERE [Name] = @Name;
+IF @Id IS NOT NULL RETURN 0;
+
+/* slow path for adding a new symbol */
+WITH [Source] AS
+(
+	SELECT
+		@Name AS [Name]
+)
+MERGE INTO [dbo].[Symbol] AS [T]
+USING [Source] AS [S]
+ON [S].[Name] = [T].[Name]
+WHEN NOT MATCHED BY TARGET THEN
+INSERT ([Name])
+VALUES ([Name])
+;
+
+SELECT @Id = [Id] FROM [Symbol] WHERE [Name] = @Name;
+RETURN 0;
+
+GO
---
+++ Trader.Data.Sql.Database/Tables/Symbol.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Symbol]
+(
+	[Id] INT NOT NULL DEFAULT NEXT VALUE FOR [dbo].[SymbolSequence],
+	[Name] NVARCHAR(100) NOT NULL,
+
+	CONSTRAINT [PK_Symbol] PRIMARY KEY CLUSTERED
+	(
+		[Id]
+	),
+
+	CONSTRAINT [UK_Symbol_Name] UNIQUE
+	(
+		[Name]
+	)
+)
+GO

ae1be2aa3c5c87382604b01b4354b84798619a8b
jorge <jorge.candeias@protonmail.com>
2021-10-14 22:08:41 +0100

    Cleanup

--- Trader.Dashboard.WebApp/Program.cs
+++ Trader.Dashboard.WebApp/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Outcompute.Trader.Dashboard.WebApp
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {

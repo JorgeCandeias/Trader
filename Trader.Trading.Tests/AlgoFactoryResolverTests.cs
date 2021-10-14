4d4426925dd7405b2c324719b86f3162b7560bdf
jorge <jorge.candeias@protonmail.com>
2021-10-15 00:46:32 +0100

    Added test

---
+++ Trader.Trading.Tests/AlgoFactoryResolverTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Orleans.Runtime;
+using Outcompute.Trader.Trading.Algorithms;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.Tests
+{
+    public class AlgoFactoryResolverTests
+    {
+        private class MyAlgo : IAlgo
+        {
+            public ValueTask GoAsync(CancellationToken cancellationToken = default) => ValueTask.CompletedTask;
+        }
+
+        [Fact]
+        public void Resolves()
+        {
+            // arrange
+            var services = new ServiceCollection();
+            services.AddSingleton(typeof(IKeyedServiceCollection<,>), typeof(KeyedServiceCollection<,>));
+            services.AddAlgoType<MyAlgo>("MyAlgo");
+            var provider = services.BuildServiceProvider();
+            var resolver = new AlgoFactoryResolver(provider);
+
+            // act
+            var factory = resolver.Resolve("MyAlgo");
+
+            // assert
+            var algo = factory.Create("Algo1");
+            Assert.IsType<MyAlgo>(algo);
+        }
+    }
+}

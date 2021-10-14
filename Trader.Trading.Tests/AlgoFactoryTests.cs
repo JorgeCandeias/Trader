a40c5c15443597ac46799490d6d3ef9911c90efc
jorge <jorge.candeias@protonmail.com>
2021-10-15 00:22:30 +0100

    Added test

---
+++ Trader.Trading.Tests/AlgoFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Outcompute.Trader.Trading.Algorithms;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.Tests
+{
+    public class AlgoFactoryTests
+    {
+        private class MyAlgo : IAlgo
+        {
+            public ValueTask GoAsync(CancellationToken cancellationToken = default) => ValueTask.CompletedTask;
+        }
+
+        [Fact]
+        public void CreatesAlgo()
+        {
+            // arrange
+            var provider = Mock.Of<IServiceProvider>();
+            var factory = new AlgoFactory<MyAlgo>(provider);
+
+            // act
+            var algo = factory.Create("Algo1");
+
+            // assert
+            Assert.IsType<MyAlgo>(algo);
+        }
+    }
+}

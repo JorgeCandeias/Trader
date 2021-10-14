607ce0049a9e7dfe21efbedd68706875e281c096
jorge <jorge.candeias@protonmail.com>
2021-10-14 23:36:54 +0100

    Added test

---
+++ Trader.Trading.Tests/AlgoContextTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using Outcompute.Trader.Trading.Algorithms;
+using System;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.Tests
+{
+    public class AlgoContextTests
+    {
+        [Fact]
+        public void Constructs()
+        {
+            // arrange
+            AlgoFactoryContext.AlgoName = "MyAlgoName";
+            var provider = Mock.Of<IServiceProvider>();
+
+            // act
+            var context = new AlgoContext(provider);
+
+            // asset
+            Assert.Same(AlgoFactoryContext.AlgoName, context.Name);
+            Assert.Same(provider, context.ServiceProvider);
+        }
+    }
+}

8aca36eb061f0867e23dd1c9e6616e6399749f2e
jorge <jorge.candeias@protonmail.com>
2021-10-15 01:02:14 +0100

    Added test

---
+++ Trader.Trading.Tests/AlgoFactoryResolverServiceCollectionExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Outcompute.Trader.Trading.Algorithms;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.Tests
+{
+    public class AlgoFactoryResolverServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddsServices()
+        {
+            // arrange
+            var services = new ServiceCollection();
+
+            // act
+            services.AddAlgoFactoryResolver();
+
+            // assert
+            var provider = services.BuildServiceProvider();
+            var resolver = provider.GetService<IAlgoFactoryResolver>();
+            Assert.IsType<AlgoFactoryResolver>(resolver);
+        }
+    }
+}

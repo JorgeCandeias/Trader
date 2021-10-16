ae24bd36516280737ee1eea84681096ca423ce9c
jorge <jorge.candeias@protonmail.com>
2021-10-17 00:06:30 +0100

    Refactored

--- Trader.Trading/Algorithms/ValueAveraging/ValueAveragingAlgo.cs
+++ Trader.Trading/Algorithms/ValueAveraging/ValueAveragingAlgo.cs
@@ -29,7 +29,6 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
         private static string TypeName => nameof(ValueAveragingAlgo);
 
         private ValueAveragingAlgoOptions _options = ValueAveragingAlgoOptions.Default;
-        private Symbol _symbol = Symbol.Empty;
         private SignificantResult _significant = SignificantResult.Empty;
         private MiniTicker _ticker = MiniTicker.Empty;
         private decimal _smaA;
@@ -43,15 +42,11 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
         {
             _options = _monitor.Get(_context.Name);
 
-            _symbol = await _context.GetRequiredSymbolAsync(_options.Symbol, cancellationToken);
-
-            _logger.LogInformation("{Type} {Name} running...", TypeName, _context.Name);
-
             // get significant orders
-            _significant = await _context.GetSignificantOrderResolver().ResolveAsync(_symbol, cancellationToken);
+            _significant = await _context.GetSignificantOrderResolver().ResolveAsync(_context.Symbol, cancellationToken);
 
             // get current ticker
-            _ticker = await _context.GetRequiredTickerAsync(_symbol.Name, cancellationToken);
+            _ticker = await _context.GetRequiredTickerAsync(_context.Symbol.Name, cancellationToken);
 
             // calculate current unrealized pnl
             if (_significant.Orders.Count > 0)
@@ -64,18 +59,18 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
 
                 _logger.LogInformation(
                     "{Type} {Name} reports Unrealized PnL = {Pnl:F8} {Asset}, Change % = {Change:P2}",
-                    TypeName, _context.Name, pnl, _symbol.QuoteAsset, percent);
+                    TypeName, _context.Name, pnl, _context.Symbol.QuoteAsset, percent);
 
                 _logger.LogInformation(
                     "{Type} {Name} reports Realized PnL = {Pnl:F8} {Asset}",
-                    TypeName, _context.Name, _significant.Profit.ThisYear, _symbol.QuoteAsset);
+                    TypeName, _context.Name, _significant.Profit.ThisYear, _context.Symbol.QuoteAsset);
             }
 
             // get the lastest klines
             var maxPeriods = GetMaxPeriods();
             var end = _clock.UtcNow;
             var start = end.Subtract(_options.KlineInterval, maxPeriods);
-            var klines = await _context.GetKlinesAsync(_symbol.Name, _options.KlineInterval, start, end, cancellationToken);
+            var klines = await _context.GetKlinesAsync(_context.Symbol.Name, _options.KlineInterval, start, end, cancellationToken);
 
             // calculate the current moving averages
             _smaA = klines.LastSimpleMovingAverage(x => x.ClosePrice, _options.SmaPeriodsA);
@@ -89,20 +84,20 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
 
             if (TrySignalBuyOrder())
             {
-                await _context.SetTrackingBuyAsync(_symbol, _options.BuyOrderSafetyRatio, _options.TargetQuoteBalanceFractionPerBuy, _options.MaxNotional, cancellationToken);
+                await _context.SetTrackingBuyAsync(_context.Symbol, _options.BuyOrderSafetyRatio, _options.TargetQuoteBalanceFractionPerBuy, _options.MaxNotional, cancellationToken);
             }
             else
             {
-                await _context.ClearOpenOrdersAsync(_symbol, OrderSide.Buy, cancellationToken);
+                await _context.ClearOpenOrdersAsync(_context.Symbol, OrderSide.Buy, cancellationToken);
             }
 
             if (TrySignalSellOrder())
             {
-                await _context.SetSignificantAveragingSellAsync(_symbol, _ticker, _significant.Orders, _options.ProfitMultipler, _options.RedeemSavings, cancellationToken);
+                await _context.SetSignificantAveragingSellAsync(_context.Symbol, _ticker, _significant.Orders, _options.ProfitMultipler, _options.RedeemSavings, cancellationToken);
             }
             else
             {
-                await _context.ClearOpenOrdersAsync(_symbol, OrderSide.Sell, cancellationToken);
+                await _context.ClearOpenOrdersAsync(_context.Symbol, OrderSide.Sell, cancellationToken);
             }
 
             // publish the profit stats
@@ -121,7 +116,7 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
             {
                 _logger.LogInformation(
                     "{Type} {Symbol} has opening disabled and will not signal a buy order",
-                    TypeName, _symbol.Name);
+                    TypeName, _context.Symbol.Name);
 
                 return false;
             }
@@ -131,7 +126,7 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
             {
                 _logger.LogInformation(
                     "{Type} {Symbol} has averaging disabled and will not signal a buy order",
-                    TypeName, _symbol.Name);
+                    TypeName, _context.Symbol.Name);
 
                 return false;
             }
@@ -145,7 +140,7 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
                 {
                     _logger.LogInformation(
                         "{Type} {Symbol} detected ticker of {Ticker:F8} is above the low price of {LowPrice:F8} calculated as {PullBackRatio:F8} of the min significant buy price of {MinPrice:F8} and will not signal a buy order",
-                        TypeName, _symbol.Name, _ticker.ClosePrice, lowPrice, _options.PullbackRatio, minPrice);
+                        TypeName, _context.Symbol.Name, _ticker.ClosePrice, lowPrice, _options.PullbackRatio, minPrice);
 
                     return false;
                 }
@@ -153,11 +148,11 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
 
             _logger.LogInformation(
                 "{Type} {Symbol} evaluating indicators (SMA({SmaPeriodsA}) = {SMAA:F8}, SMA({SmaPeriodsB}) = {SMAB:F8}, SMA({SmaPeriodsC}) = {SMAC:F8})",
-                TypeName, _symbol.Name, _options.SmaPeriodsA, _smaA, _options.SmaPeriodsB, _smaB, _options.SmaPeriodsC, _smaC);
+                TypeName, _context.Symbol.Name, _options.SmaPeriodsA, _smaA, _options.SmaPeriodsB, _smaB, _options.SmaPeriodsC, _smaC);
 
             _logger.LogInformation(
                  "{Type} {Symbol} evaluating indicators (RSI({RsiPeriodsA}) = {RSIA:F8}, RSI({RsiPeriodsB}) = {RSIB:F8}, RSI({RsiPeriodsC}) = {RSIC:F8})",
-                 TypeName, _symbol.Name, _options.RsiPeriodsA, _rsiA, _options.RsiPeriodsB, _rsiB, _options.RsiPeriodsC, _rsiC);
+                 TypeName, _context.Symbol.Name, _options.RsiPeriodsA, _rsiA, _options.RsiPeriodsB, _rsiB, _options.RsiPeriodsC, _rsiC);
 
             // evaluate the signal now
             var isSmaOrdered = _smaA < _smaB && _smaB < _smaC;
@@ -170,7 +165,7 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
             {
                 _logger.LogInformation(
                     "{Type} {Symbol} will signal a buy order for the current state (Ticker = {Ticker:F8}, SMA({SmaPeriodsA}) = {SMAA:F8}, SMA({SmaPeriodsB}) = {SMAB:F8}, SMA({SmaPeriodsC}) = {SMAC:F8}, RSI({RsiPeriodsA}) = {RSIA:F8}, RSI({RsiPeriodsB}) = {RSIB:F8}, RSI({RsiPeriodsC}) = {RSIC:F8})",
-                    TypeName, _symbol.Name, _ticker.ClosePrice, _options.SmaPeriodsA, _smaA, _options.SmaPeriodsB, _smaB, _options.SmaPeriodsC, _smaC, _options.RsiPeriodsA, _rsiA, _options.RsiPeriodsB, _rsiB, _options.RsiPeriodsC, _rsiC);
+                    TypeName, _context.Symbol.Name, _ticker.ClosePrice, _options.SmaPeriodsA, _smaA, _options.SmaPeriodsB, _smaB, _options.SmaPeriodsC, _smaC, _options.RsiPeriodsA, _rsiA, _options.RsiPeriodsB, _rsiB, _options.RsiPeriodsC, _rsiC);
             }
 
             return signal;
@@ -187,7 +182,7 @@ namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
             {
                 _logger.LogInformation(
                     "{Type} {Symbol} will signal a sell order for the current state (Ticker = {Ticker:F8}, SMA({SmaPeriodsA}) = {SMAA:F8}, SMA({SmaPeriodsB}) = {SMAB:F8}, SMA({SmaPeriodsC}) = {SMAC:F8}, RSI({RsiPeriodsA}) = {RSIA:F8}, RSI({RsiPeriodsB}) = {RSIB:F8}, RSI({RsiPeriodsC}) = {RSIC:F8})",
-                    TypeName, _symbol.Name, _ticker.ClosePrice, _options.SmaPeriodsA, _smaA, _options.SmaPeriodsB, _smaB, _options.SmaPeriodsC, _smaC, _options.RsiPeriodsA, _rsiA, _options.RsiPeriodsB, _rsiB, _options.RsiPeriodsC, _rsiC);
+                    TypeName, _context.Symbol.Name, _ticker.ClosePrice, _options.SmaPeriodsA, _smaA, _options.SmaPeriodsB, _smaB, _options.SmaPeriodsC, _smaC, _options.RsiPeriodsA, _rsiA, _options.RsiPeriodsB, _rsiB, _options.RsiPeriodsC, _rsiC);
             }
 
             return signal;
--- Trader.Trading/Algorithms/ValueAveraging/ValueAveragingAlgoOptions.cs
+++ Trader.Trading/Algorithms/ValueAveraging/ValueAveragingAlgoOptions.cs
@@ -1,14 +1,10 @@
 ﻿using Outcompute.Trader.Models;
 using System.ComponentModel.DataAnnotations;
-using static System.String;
 
 namespace Outcompute.Trader.Trading.Algorithms.ValueAveraging
 {
     public class ValueAveragingAlgoOptions
     {
-        [Required]
-        public string Symbol { get; set; } = Empty;
-
         [Required]
         [Range(0.01, 0.99)]
         public decimal PullbackRatio { get; set; } = 0.90m;

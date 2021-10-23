16d8cdd4d4c06c92adedc8fa8fd0378fae0b2c58
jorge <jorge.candeias@protonmail.com>
2021-10-23 19:07:48 +0100

    Added test

---
+++ Trader.Core.Tests/DisposableActionTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Outcompute.Trader.Core.Tests
+{
+    public class DisposableActionTests
+    {
+        [Fact]
+        public void ActionOnDispose()
+        {
+            // arrange
+            var okay = false;
+            var action = new DisposableAction(() => okay = true);
+
+            // act
+            action.Dispose();
+
+            // assert
+            Assert.True(okay);
+        }
+    }
+}

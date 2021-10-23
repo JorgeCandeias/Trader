bd2967241754a627f45dd9d476508b6447ef4430
jorge <jorge.candeias@protonmail.com>
2021-10-23 21:57:56 +0100

    Added tests

---
+++ Trader.Trading.Tests/AlgoOptionsConfiguratorTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Outcompute.Trader.Trading.Algorithms;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.Tests
+{
+    public class AlgoOptionsConfiguratorTests
+    {
+        private class TestOptions
+        {
+            public string MyValue { get; set; } = string.Empty;
+        }
+
+        [Fact]
+        public void ConfiguresNamedOptions()
+        {
+            // arrange
+            var name = "TestName";
+            var value = "TestValue";
+            var mapping = new AlgoConfigurationMappingOptions();
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { $"{mapping.AlgosKey}:{name}:{mapping.AlgoOptionsSubKey}:MyValue", value }
+                })
+                .Build();
+            var configurator = new AlgoOptionsConfigurator<TestOptions>(Options.Create(mapping), config);
+            var options = new TestOptions();
+
+            // act
+            configurator.Configure(name, options);
+
+            // assert
+            Assert.Equal(value, options.MyValue);
+        }
+
+        [Fact]
+        public void ThrowsOnDefaultOptions()
+        {
+            // arrange
+            var name = "TestName";
+            var value = "TestValue";
+            var mapping = new AlgoConfigurationMappingOptions();
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { $"{mapping.AlgosKey}:{name}:{mapping.AlgoOptionsSubKey}:MyValue", value }
+                })
+                .Build();
+            var configurator = new AlgoOptionsConfigurator<TestOptions>(Options.Create(mapping), config);
+            var options = new TestOptions();
+
+            // act
+            void Test() => configurator.Configure(options);
+
+            // assert
+            Assert.Throws<NotSupportedException>(Test);
+        }
+    }
+}

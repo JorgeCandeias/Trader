5ae0453072cc7ebc7964a19fc6edb9791d77cea7
jorge <jorge.candeias@protonmail.com>
2021-10-23 22:01:35 +0100

    Added test

---
+++ Trader.Trading.Tests/BalanceProviderAlgoContextExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Outcompute.Trader.Trading.Algorithms;
+using Outcompute.Trader.Trading.Providers;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.Tests
+{
+    public class BalanceProviderAlgoContextExtensionsTests
+    {
+        [Fact]
+        public void GetBalanceProvider()
+        {
+            // arrange
+            var balances = Mock.Of<IBalanceProvider>();
+
+            var provider = new ServiceCollection()
+                .AddSingleton(balances)
+                .BuildServiceProvider();
+
+            var context = Mock.Of<IAlgoContext>(x =>
+                x.ServiceProvider == provider);
+
+            // act
+            var result = context.GetBalanceProvider();
+
+            // assert
+            Assert.Same(balances, result);
+        }
+    }
+}

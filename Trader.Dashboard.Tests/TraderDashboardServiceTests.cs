2c1cbe637937e0e4cad6e49ed3610a88988c6826
jorge <jorge.candeias@protonmail.com>
2021-10-25 19:54:34 +0100

    Added test

---
+++ Trader.Dashboard.Tests/TraderDashboardServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Outcompute.Trader.Dashboard;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Trader.Dashboard.Tests
+{
+    public class TraderDashboardServiceTests
+    {
+        [Fact]
+        public async Task Cycles()
+        {
+            // arrange
+            var options = new TraderDashboardOptions();
+            var loggerProviders = Enumerable.Empty<ILoggerProvider>();
+            var provider = Mock.Of<IServiceProvider>();
+            using var service = new TraderDashboardService(Options.Create(options), loggerProviders, provider);
+
+            // act
+            await service.StartAsync(CancellationToken.None);
+            await service.StopAsync(CancellationToken.None);
+
+            // assert
+            Assert.True(true);
+        }
+    }
+}

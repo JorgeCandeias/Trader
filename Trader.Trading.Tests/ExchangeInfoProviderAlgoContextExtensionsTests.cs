50b282703f4516b98aa23db9fd97a1de03b17ede
jorge <jorge.candeias@protonmail.com>
2021-10-26 02:19:14 +0100

    Added test

---
+++ Trader.Trading.Tests/ExchangeInfoProviderAlgoContextExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Outcompute.Trader.Trading.Algorithms;
+using Outcompute.Trader.Trading.Providers;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.Tests
+{
+    public class ExchangeInfoProviderAlgoContextExtensionsTests
+    {
+        [Fact]
+        public void GetExchangeInfoProvider()
+        {
+            // arrange
+            var exchange = Mock.Of<IExchangeInfoProvider>();
+            var provider = new ServiceCollection()
+                .AddSingleton(exchange)
+                .BuildServiceProvider();
+            var context = new AlgoContext(provider);
+
+            // act
+            var result = context.GetExchangeInfoProvider();
+
+            // assert
+            Assert.Same(exchange, result);
+        }
+    }
+}

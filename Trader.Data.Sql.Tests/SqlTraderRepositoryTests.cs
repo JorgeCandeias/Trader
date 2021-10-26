10dd904dee9192be792ffa16e4da98c75b9174bb
jorge <jorge.candeias@protonmail.com>
2021-10-26 20:16:54 +0100

    Added test

--- Trader.Data.Sql.Tests/SqlTraderRepositoryTests.cs
+++ Trader.Data.Sql.Tests/SqlTraderRepositoryTests.cs
@@ -1,6 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
+using Outcompute.Trader.Models;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+using Xunit;
 
 namespace Outcompute.Trader.Data.Sql.Tests
 {
@@ -19,6 +24,35 @@ namespace Outcompute.Trader.Data.Sql.Tests
                 Options.Create(new SqlTradingRepositoryOptions { ConnectionString = ConnectionString }),
                 NullLogger<SqlTradingRepository>.Instance,
                 _mapper);
+        }
+
+        private readonly SqlTradingRepository _repository;
+
+        public SqlTraderRepositoryTests()
+        {
+            _repository = CreateRepository();
+        }
+
+        [Fact]
+        public async Task SetsAndGetsBalances()
+        {
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            // arrange
+            var balance1 = new Balance(Guid.NewGuid().ToString(), 111, 222, DateTime.UtcNow);
+            var balance2 = new Balance(Guid.NewGuid().ToString(), 333, 444, DateTime.UtcNow);
+            var balances = new[] { balance1, balance2 };
+
+            // act
+            await _repository.SetBalancesAsync(balances);
+            var result1 = await _repository.TryGetBalanceAsync(balance1.Asset);
+            var result2 = await _repository.TryGetBalanceAsync(balance2.Asset);
+            var result3 = await _repository.TryGetBalanceAsync(Guid.NewGuid().ToString());
+
+            // assert
+            Assert.Equal(balance1, result1);
+            Assert.Equal(balance2, result2);
+            Assert.Null(result3);
         }
     }
 }

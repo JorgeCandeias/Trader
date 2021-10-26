a033978e47f300e9f570d3dc3bc5ac688880f1f0
jorge <jorge.candeias@protonmail.com>
2021-10-26 02:35:20 +0100

    Added test

---
+++ Trader.Trading.InMemory.Tests/InMemoryUserDataStreamSenderTests.cs
@@ -0,0 +1,33 @@
+﻿using Outcompute.Trader.Models;
+using Outcompute.Trader.Trading.InMemory.UserData;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.InMemory.Tests
+{
+    public class InMemoryUserDataStreamSenderTests
+    {
+        [Fact]
+        public async Task Sends()
+        {
+            // arrange
+            var sender = new InMemoryUserDataStreamSender();
+            var message = new UserDataStreamMessage();
+
+            // act
+            UserDataStreamMessage? received = null;
+            var completion = new TaskCompletionSource();
+            using var reg = sender.Register((m, ct) =>
+            {
+                received = m;
+                completion.SetResult();
+                return Task.CompletedTask;
+            });
+            await sender.SendAsync(message);
+
+            // assert
+            await Task.WhenAny(completion.Task, Task.Delay(100));
+            Assert.Same(message, received);
+        }
+    }
+}

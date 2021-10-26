27905cb73215fac25e12d19660ee5dee103bc28c
jorge <jorge.candeias@protonmail.com>
2021-10-26 02:24:43 +0100

    Added test

---
+++ Trader.Trading.InMemory.Tests/InMemoryUserDataStreamClientTests.cs
@@ -0,0 +1,30 @@
+﻿using Outcompute.Trader.Models;
+using Outcompute.Trader.Trading.InMemory.UserData;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Outcompute.Trader.Trading.InMemory.Tests
+{
+    public class InMemoryUserDataStreamClientTests
+    {
+        [Fact]
+        public async Task Cycles()
+        {
+            // arrange
+            var message = new UserDataStreamMessage();
+            var sender = new InMemoryUserDataStreamSender();
+            using var client = new InMemoryUserDataStreamClient(sender);
+
+            // act
+            await client.ConnectAsync();
+            await sender.SendAsync(message);
+            var result = await client.ReceiveAsync();
+
+            // assert
+            Assert.Same(message, result);
+
+            // act
+            await client.CloseAsync();
+        }
+    }
+}

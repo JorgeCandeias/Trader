d51d186763e8f8f2489420313e2f43bf468eb677
jorge <jorge.candeias@protonmail.com>
2021-10-28 23:19:49 +0100

    Added test

--- Trader.Trading.Tests/SteppingAlgoTests.cs
+++ Trader.Trading.Tests/SteppingAlgoTests.cs
@@ -5,7 +5,9 @@ using Moq;
 using Outcompute.Trader.Models;
 using Outcompute.Trader.Trading.Algorithms;
 using Outcompute.Trader.Trading.Algorithms.Stepping;
+using Outcompute.Trader.Trading.Commands;
 using Outcompute.Trader.Trading.Commands.ClearOpenOrders;
+using Outcompute.Trader.Trading.Commands.RedeemSavings;
 using Outcompute.Trader.Trading.Providers;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -59,6 +61,72 @@ namespace Outcompute.Trader.Trading.Tests
             var command = Assert.IsType<ClearOpenOrdersCommand>(result);
             Assert.Equal(symbol, command.Symbol);
             Assert.Equal(OrderSide.Buy, command.Side);
+        }
+
+        [Fact]
+        public async Task ReturnsClearOpenOrdersOnDisabledOpening2()
+        {
+            // arrange
+            var name = "MyAlgo";
+            var logger = NullLogger<SteppingAlgo>.Instance;
+            var options = Mock.Of<IOptionsMonitor<SteppingAlgoOptions>>(
+                x => x.Get(name).IsOpeningEnabled);
+
+            var symbol = Symbol.Empty with
+            {
+                Name = "ABCXYZ",
+                BaseAsset = "ABC",
+                QuoteAsset = "XYZ",
+                Filters = SymbolFilters.Empty with
+                {
+                    Price = PriceSymbolFilter.Empty with
+                    {
+                        TickSize = 0.0000001m
+                    },
+                    MinNotional = MinNotionalSymbolFilter.Empty with
+                    {
+                        MinNotional = 0.00010000m
+                    }
+                }
+            };
+
+            var orderProvider = Mock.Of<IOrderProvider>();
+            Mock.Get(orderProvider)
+                .Setup(x => x.GetOrdersAsync(symbol.Name, CancellationToken.None))
+                .Returns(Task.FromResult<IReadOnlyList<OrderQueryResult>>(ImmutableList<OrderQueryResult>.Empty))
+                .Verifiable();
+
+            var algo = new SteppingAlgo(logger, options, orderProvider);
+
+            var redeemSavingsExecutor = Mock.Of<IAlgoCommandExecutor<RedeemSavingsCommand, RedeemSavingsEvent>>();
+            Mock.Get(redeemSavingsExecutor)
+                .Setup(x => x.ExecuteAsync(It.IsAny<IAlgoContext>(), It.Is<RedeemSavingsCommand>(x => x.Asset == symbol.QuoteAsset && x.Amount == 0.00010000m), CancellationToken.None))
+                .Returns(Task.FromResult(new RedeemSavingsEvent(true, 0m)))
+                .Verifiable();
+
+            var provider = new ServiceCollection()
+                .AddSingleton(redeemSavingsExecutor)
+                .BuildServiceProvider();
+
+            algo.Context = new AlgoContext(provider)
+            {
+                Name = name,
+                Symbol = symbol,
+                Ticker = MiniTicker.Empty with
+                {
+                    Symbol = symbol.Name,
+                    ClosePrice = 10000m
+                }
+            };
+
+            // act
+            var result = await algo.GoAsync();
+
+            // arrange
+            Mock.Get(orderProvider).VerifyAll();
+            Mock.Get(redeemSavingsExecutor).VerifyAll();
+
+            Assert.IsType<NoopAlgoCommand>(result);
         }
     }
 }

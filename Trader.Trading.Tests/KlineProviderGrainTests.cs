c221c7cd7ababac51e44a4db45bd83c52ef0f764
jorge <jorge.candeias@protonmail.com>
2021-10-28 19:01:27 +0100

    Added test

--- Trader.Trading.Tests/KlineProviderGrainTests.cs
+++ Trader.Trading.Tests/KlineProviderGrainTests.cs
@@ -46,5 +46,41 @@ namespace Outcompute.Trader.Trading.Tests
             Assert.Equal(item.Interval, result.Interval);
             Assert.Equal(item.OpenTime, result.OpenTime);
         }
+
+        [Fact]
+        public async Task CompletesPromises()
+        {
+            // arrange
+            var symbol = "ABCXYZ";
+            var interval = KlineInterval.Days1;
+            var options = Options.Create(new KlineProviderOptions());
+            var reactive = Options.Create(new ReactiveOptions());
+            var repository = Mock.Of<ITradingRepository>();
+            var clock = Mock.Of<ISystemClock>();
+            var lifetime = Mock.Of<IHostApplicationLifetime>();
+            var grain = new KlineProviderGrain(options, reactive, repository, clock, lifetime)
+            {
+                _symbol = symbol,
+                _interval = interval
+            };
+            var item = Kline.Empty with
+            {
+                Symbol = symbol,
+                Interval = interval,
+                OpenTime = DateTime.UtcNow.Date
+            };
+
+            // act
+            var current = await grain.GetKlinesAsync();
+            var promise = grain.TryWaitForKlinesAsync(current.Version, current.Serial + 1);
+            await grain.SetKlineAsync(item);
+            var result = await promise;
+
+            // assert
+            Assert.True(result.HasValue);
+            Assert.Collection(result.Value.Items, x => Assert.Equal(item, x));
+            Assert.Equal(current.Serial + 1, result.Value.Serial);
+            Assert.Equal(current.Version, result.Value.Version);
+        }
     }
 }

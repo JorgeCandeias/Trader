b61d2a6fbde5c33611ae2766e7c126902f4b023f
jorge <jorge.candeias@protonmail.com>
2021-10-29 22:00:04 +0100

    Added test

--- Trader.Trading.Tests/SignificantAveragingSellExecutorTests.cs
+++ Trader.Trading.Tests/SignificantAveragingSellExecutorTests.cs
@@ -5,6 +5,7 @@ using Outcompute.Trader.Models;
 using Outcompute.Trader.Trading.Algorithms;
 using Outcompute.Trader.Trading.Commands;
 using Outcompute.Trader.Trading.Commands.ClearOpenOrders;
+using Outcompute.Trader.Trading.Commands.EnsureSingleOrder;
 using Outcompute.Trader.Trading.Commands.SignificantAveragingSell;
 using System;
 using System.Threading;
@@ -99,6 +100,215 @@ namespace Outcompute.Trader.Trading.Tests
             // assert
             Mock.Get(clearOpenOrdersExecutor)
                 .Verify(x => x.ExecuteAsync(context, It.Is<ClearOpenOrdersCommand>(x => x.Side == OrderSide.Sell && x.Symbol == symbol), CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task ClearsSellOrdersOnSellableQuantityUnderMinLotSize()
+        {
+            // arrange
+            var logger = NullLogger<SignificantAveragingSellExecutor>.Instance;
+            var executor = new SignificantAveragingSellExecutor(logger);
+            var clearOpenOrdersExecutor = Mock.Of<IAlgoCommandExecutor<ClearOpenOrdersCommand>>();
+            var provider = new ServiceCollection()
+                .AddSingleton(clearOpenOrdersExecutor)
+                .BuildServiceProvider();
+            var context = new AlgoContext(provider);
+            var symbol = Symbol.Empty with
+            {
+                Name = "ABCXYZ",
+                BaseAsset = "ABC",
+                QuoteAsset = "XYZ",
+                Filters = SymbolFilters.Empty with
+                {
+                    Price = PriceSymbolFilter.Empty with
+                    {
+                        TickSize = 1m
+                    },
+                    LotSize = LotSizeSymbolFilter.Empty with
+                    {
+                        StepSize = 1m,
+                        MinQuantity = 1000m
+                    }
+                }
+            };
+            var ticker = MiniTicker.Empty with
+            {
+                Symbol = symbol.Name,
+                ClosePrice = 50000
+            };
+            var orders = new[]
+            {
+                OrderQueryResult.Empty with
+                {
+                    Symbol = symbol.Name,
+                    Side = OrderSide.Buy,
+                    ExecutedQuantity = 100m,
+                    Price = 50000m
+                },
+                OrderQueryResult.Empty with
+                {
+                    Symbol = symbol.Name,
+                    Side = OrderSide.Buy,
+                    ExecutedQuantity = 100m,
+                    Price = 40000m
+                }
+            };
+            var minimumProfitRate = 1.01m;
+            var redeemSavings = false;
+            var command = new SignificantAveragingSellCommand(symbol, ticker, orders, minimumProfitRate, redeemSavings);
+
+            // act
+            await executor.ExecuteAsync(context, command, CancellationToken.None);
+
+            // assert
+            Mock.Get(clearOpenOrdersExecutor)
+                .Verify(x => x.ExecuteAsync(context, It.Is<ClearOpenOrdersCommand>(x => x.Side == OrderSide.Sell && x.Symbol == symbol), CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task ClearsSellOrdersOnSellableQuantityUnderMinNotional()
+        {
+            // arrange
+            var logger = NullLogger<SignificantAveragingSellExecutor>.Instance;
+            var executor = new SignificantAveragingSellExecutor(logger);
+            var clearOpenOrdersExecutor = Mock.Of<IAlgoCommandExecutor<ClearOpenOrdersCommand>>();
+            var provider = new ServiceCollection()
+                .AddSingleton(clearOpenOrdersExecutor)
+                .BuildServiceProvider();
+            var context = new AlgoContext(provider);
+            var symbol = Symbol.Empty with
+            {
+                Name = "ABCXYZ",
+                BaseAsset = "ABC",
+                QuoteAsset = "XYZ",
+                Filters = SymbolFilters.Empty with
+                {
+                    Price = PriceSymbolFilter.Empty with
+                    {
+                        TickSize = 1m
+                    },
+                    LotSize = LotSizeSymbolFilter.Empty with
+                    {
+                        StepSize = 1m,
+                        MinQuantity = 100m
+                    },
+                    MinNotional = MinNotionalSymbolFilter.Empty with
+                    {
+                        MinNotional = decimal.MaxValue
+                    }
+                }
+            };
+            var ticker = MiniTicker.Empty with
+            {
+                Symbol = symbol.Name,
+                ClosePrice = 50000
+            };
+            var orders = new[]
+            {
+                OrderQueryResult.Empty with
+                {
+                    Symbol = symbol.Name,
+                    Side = OrderSide.Buy,
+                    ExecutedQuantity = 100m,
+                    Price = 50000m
+                },
+                OrderQueryResult.Empty with
+                {
+                    Symbol = symbol.Name,
+                    Side = OrderSide.Buy,
+                    ExecutedQuantity = 100m,
+                    Price = 40000m
+                }
+            };
+            var minimumProfitRate = 1.01m;
+            var redeemSavings = false;
+            var command = new SignificantAveragingSellCommand(symbol, ticker, orders, minimumProfitRate, redeemSavings);
+
+            // act
+            await executor.ExecuteAsync(context, command, CancellationToken.None);
+
+            // assert
+            Mock.Get(clearOpenOrdersExecutor)
+                .Verify(x => x.ExecuteAsync(context, It.Is<ClearOpenOrdersCommand>(x => x.Side == OrderSide.Sell && x.Symbol == symbol), CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task EnsuresSingleOrderOnSellableOrders()
+        {
+            // arrange
+            var logger = NullLogger<SignificantAveragingSellExecutor>.Instance;
+            var executor = new SignificantAveragingSellExecutor(logger);
+            var clearOpenOrdersExecutor = Mock.Of<IAlgoCommandExecutor<ClearOpenOrdersCommand>>();
+            var ensureSingleOrderExecutor = Mock.Of<IAlgoCommandExecutor<EnsureSingleOrderCommand>>();
+            var provider = new ServiceCollection()
+                .AddSingleton(clearOpenOrdersExecutor)
+                .AddSingleton(ensureSingleOrderExecutor)
+                .BuildServiceProvider();
+            var context = new AlgoContext(provider);
+            var symbol = Symbol.Empty with
+            {
+                Name = "ABCXYZ",
+                BaseAsset = "ABC",
+                QuoteAsset = "XYZ",
+                Filters = SymbolFilters.Empty with
+                {
+                    Price = PriceSymbolFilter.Empty with
+                    {
+                        TickSize = 1m
+                    },
+                    LotSize = LotSizeSymbolFilter.Empty with
+                    {
+                        StepSize = 1m,
+                        MinQuantity = 100m
+                    },
+                    MinNotional = MinNotionalSymbolFilter.Empty with
+                    {
+                        MinNotional = 1m
+                    }
+                }
+            };
+            var ticker = MiniTicker.Empty with
+            {
+                Symbol = symbol.Name,
+                ClosePrice = 50000
+            };
+            var orders = new[]
+            {
+                OrderQueryResult.Empty with
+                {
+                    Symbol = symbol.Name,
+                    Side = OrderSide.Buy,
+                    ExecutedQuantity = 100m,
+                    Price = 50000m
+                },
+                OrderQueryResult.Empty with
+                {
+                    Symbol = symbol.Name,
+                    Side = OrderSide.Buy,
+                    ExecutedQuantity = 100m,
+                    Price = 40000m
+                }
+            };
+            var minimumProfitRate = 1.01m;
+            var redeemSavings = false;
+            var command = new SignificantAveragingSellCommand(symbol, ticker, orders, minimumProfitRate, redeemSavings);
+
+            // act
+            await executor.ExecuteAsync(context, command, CancellationToken.None);
+
+            // assert
+            Mock.Get(ensureSingleOrderExecutor)
+                .Verify(x => x.ExecuteAsync(
+                    context,
+                    It.Is<EnsureSingleOrderCommand>(x =>
+                        x.Symbol == symbol &&
+                        x.Side == OrderSide.Sell &&
+                        x.Type == OrderType.Limit &&
+                        x.TimeInForce == TimeInForce.GoodTillCanceled &&
+                        x.Quantity == 200m &&
+                        x.Price == 50000m &&
+                        !x.RedeemSavings),
+                    CancellationToken.None));
         }
     }
 }

fbbba7807242dc300f8a67c9136f43a5f1f2d722
jorge <jorge.candeias@protonmail.com>
2021-10-29 21:43:58 +0100

    Added test

--- Trader.Trading.Tests/SignificantAveragingSellExecutorTests.cs
+++ Trader.Trading.Tests/SignificantAveragingSellExecutorTests.cs
@@ -6,7 +6,7 @@ using Outcompute.Trader.Trading.Algorithms;
 using Outcompute.Trader.Trading.Commands;
 using Outcompute.Trader.Trading.Commands.ClearOpenOrders;
 using Outcompute.Trader.Trading.Commands.SignificantAveragingSell;
-using System.Collections.Immutable;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -37,7 +37,58 @@ namespace Outcompute.Trader.Trading.Tests
                 Symbol = symbol.Name,
                 ClosePrice = 50000
             };
-            var orders = ImmutableSortedSet<OrderQueryResult>.Empty;
+            var orders = Array.Empty<OrderQueryResult>();
+            var minimumProfitRate = 1.01m;
+            var redeemSavings = false;
+            var command = new SignificantAveragingSellCommand(symbol, ticker, orders, minimumProfitRate, redeemSavings);
+
+            // act
+            await executor.ExecuteAsync(context, command, CancellationToken.None);
+
+            // assert
+            Mock.Get(clearOpenOrdersExecutor)
+                .Verify(x => x.ExecuteAsync(context, It.Is<ClearOpenOrdersCommand>(x => x.Side == OrderSide.Sell && x.Symbol == symbol), CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task ClearsSellOrdersOnNoSellableBuyOrders()
+        {
+            // arrange
+            var logger = NullLogger<SignificantAveragingSellExecutor>.Instance;
+            var executor = new SignificantAveragingSellExecutor(logger);
+            var clearOpenOrdersExecutor = Mock.Of<IAlgoCommandExecutor<ClearOpenOrdersCommand>>();
+            var provider = new ServiceCollection()
+                .AddSingleton(clearOpenOrdersExecutor)
+                .BuildServiceProvider();
+            var context = new AlgoContext(provider);
+            var symbol = Symbol.Empty with
+            {
+                Name = "ABCXYZ",
+                BaseAsset = "ABC",
+                QuoteAsset = "XYZ",
+                Filters = SymbolFilters.Empty with
+                {
+                    Price = PriceSymbolFilter.Empty with
+                    {
+                        TickSize = 1m
+                    }
+                }
+            };
+            var ticker = MiniTicker.Empty with
+            {
+                Symbol = symbol.Name,
+                ClosePrice = 50000
+            };
+            var orders = new[]
+            {
+                OrderQueryResult.Empty with
+                {
+                    Symbol = symbol.Name,
+                    Side = OrderSide.Buy,
+                    ExecutedQuantity = 100m,
+                    Price = 60000m
+                }
+            };
             var minimumProfitRate = 1.01m;
             var redeemSavings = false;
             var command = new SignificantAveragingSellCommand(symbol, ticker, orders, minimumProfitRate, redeemSavings);

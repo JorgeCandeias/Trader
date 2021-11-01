6d775716dc6535b4db6c85931367e2833981bbc3
jorge <jorge.candeias@protonmail.com>
2021-11-01 22:41:28 +0000

    Refactored

--- Trader.Trading/Commands/SignificantAveragingSell/SignificantAveragingSellExecutor.cs
+++ Trader.Trading/Commands/SignificantAveragingSell/SignificantAveragingSellExecutor.cs
@@ -3,7 +3,6 @@ using Outcompute.Trader.Models;
 using Outcompute.Trader.Trading.Algorithms;
 using Outcompute.Trader.Trading.Commands.ClearOpenOrders;
 using Outcompute.Trader.Trading.Commands.EnsureSingleOrder;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,7 +23,7 @@ namespace Outcompute.Trader.Trading.Commands.SignificantAveragingSell
         public Task ExecuteAsync(IAlgoContext context, SignificantAveragingSellCommand command, CancellationToken cancellationToken = default)
         {
             // calculate the desired sell
-            var desired = CalculateDesiredSell(command.Symbol, command.MinimumProfitRate, command.Orders, command.Ticker);
+            var desired = CalculateDesiredSell(command);
 
             // apply the desired sell
             if (desired == DesiredSell.None)
@@ -39,10 +38,10 @@ namespace Outcompute.Trader.Trading.Commands.SignificantAveragingSell
             }
         }
 
-        private DesiredSell CalculateDesiredSell(Symbol symbol, decimal minimumProfitRate, IReadOnlyCollection<OrderQueryResult> orders, MiniTicker ticker)
+        private DesiredSell CalculateDesiredSell(SignificantAveragingSellCommand command)
         {
             // skip if there is nothing to sell
-            if (orders.Count == 0)
+            if (command.Orders.Count == 0)
             {
                 return DesiredSell.None;
             }
@@ -52,7 +51,7 @@ namespace Outcompute.Trader.Trading.Commands.SignificantAveragingSell
             var numerator = 0m;
             var quantity = 0m;
 
-            foreach (var order in orders.Reverse())
+            foreach (var order in command.Orders.Reverse())
             {
                 // calculate the candidate average sell price
                 var orderNumerator = order.ExecutedQuantity * order.Price;
@@ -60,13 +59,13 @@ namespace Outcompute.Trader.Trading.Commands.SignificantAveragingSell
                 var candidateNumerator = numerator + orderNumerator;
                 var candidateQuantity = quantity + orderQuantity;
                 var candidateAverageBuyPrice = candidateNumerator / candidateQuantity;
-                var candidateSellPrice = candidateAverageBuyPrice * minimumProfitRate;
+                var candidateSellPrice = candidateAverageBuyPrice * command.MinimumProfitRate;
 
                 // adjust the candidate average sell price up to the tick size
-                candidateSellPrice = candidateSellPrice.AdjustPriceUpToTickSize(symbol);
+                candidateSellPrice = candidateSellPrice.AdjustPriceUpToTickSize(command.Symbol);
 
                 // elect the order if the candidate average sell price is below the ticker
-                if (candidateSellPrice <= ticker.ClosePrice)
+                if (candidateSellPrice <= command.Ticker.ClosePrice)
                 {
                     count++;
                     numerator = candidateNumerator;
@@ -74,7 +73,7 @@ namespace Outcompute.Trader.Trading.Commands.SignificantAveragingSell
 
                     _logger.LogInformation(
                         "{Type} {Symbol} elected order {OrderId} for sale with significant quantity {Quantity:F8} {Asset} at buy price {Price:F8} {Quote}",
-                        TypeName, symbol.Name, order.OrderId, order.ExecutedQuantity, symbol.BaseAsset, order.Price, symbol.QuoteAsset);
+                        TypeName, command.Symbol.Name, order.OrderId, order.ExecutedQuantity, command.Symbol.BaseAsset, order.Price, command.Symbol.QuoteAsset);
                 }
                 else
                 {
@@ -87,7 +86,7 @@ namespace Outcompute.Trader.Trading.Commands.SignificantAveragingSell
             {
                 _logger.LogInformation(
                     "{Type} {Symbol} cannot elect any buy orders for selling at a minimum profit rate of {MinimumProfitRate:F8}",
-                    TypeName, symbol.Name, minimumProfitRate);
+                    TypeName, command.Symbol.Name, command.MinimumProfitRate);
 
                 return DesiredSell.None;
             }
@@ -97,44 +96,44 @@ namespace Outcompute.Trader.Trading.Commands.SignificantAveragingSell
 
             _logger.LogInformation(
                 "{Type} {Symbol} elected {Count} orders for sale with total quantity {Quantity:F8} {Asset} at average buy price {Price:F8} {Quote}",
-                TypeName, symbol.Name, count, quantity, symbol.BaseAsset, averagePrice, symbol.QuoteAsset);
+                TypeName, command.Symbol.Name, count, quantity, command.Symbol.BaseAsset, averagePrice, command.Symbol.QuoteAsset);
 
             // adjust the quantity down to the lot size filter
-            quantity = quantity.AdjustQuantityDownToLotStepSize(symbol);
+            quantity = quantity.AdjustQuantityDownToLotStepSize(command.Symbol);
 
             _logger.LogInformation(
                 "{Type} {Symbol} adjusted quantity by lot step size of {LotStepSize} {Asset} down to {Quantity:F8} {Asset}",
-                TypeName, symbol.Name, symbol.Filters.LotSize.StepSize, symbol.BaseAsset, quantity, symbol.BaseAsset);
+                TypeName, command.Symbol.Name, command.Symbol.Filters.LotSize.StepSize, command.Symbol.BaseAsset, quantity, command.Symbol.BaseAsset);
 
             // break if the quantity is under the minimum lot size
-            if (quantity < symbol.Filters.LotSize.MinQuantity)
+            if (quantity < command.Symbol.Filters.LotSize.MinQuantity)
             {
                 _logger.LogError(
                     "{Type} {Name} cannot set sell order for {Quantity} {Asset} because the quantity is under the minimum lot size of {MinLotSize} {Asset}",
-                    TypeName, symbol.Name, quantity, symbol.BaseAsset, symbol.Filters.LotSize.MinQuantity, symbol.BaseAsset);
+                    TypeName, command.Symbol.Name, quantity, command.Symbol.BaseAsset, command.Symbol.Filters.LotSize.MinQuantity, command.Symbol.BaseAsset);
 
                 return DesiredSell.None;
             }
 
             // calculate the sell notional
-            var total = quantity * ticker.ClosePrice;
+            var total = quantity * command.Ticker.ClosePrice;
 
             _logger.LogInformation(
                 "{Type} {Symbol} calculated notional of {Total:F8} {Quote} using quantity of {Quantity:F8} {Asset} and ticker of {Price:F8} {Quote}",
-                TypeName, symbol.Name, total, symbol.QuoteAsset, quantity, symbol.BaseAsset, ticker.ClosePrice, symbol.QuoteAsset);
+                TypeName, command.Symbol.Name, total, command.Symbol.QuoteAsset, quantity, command.Symbol.BaseAsset, command.Ticker.ClosePrice, command.Symbol.QuoteAsset);
 
             // check if the sell is under the minimum notional filter
-            if (total < symbol.Filters.MinNotional.MinNotional)
+            if (total < command.Symbol.Filters.MinNotional.MinNotional)
             {
                 _logger.LogError(
                     "{Type} {Name} cannot set sell order for {Quantity} {Asset} at {Price} {Quote} totalling {Total} {Quote} because it is under the minimum notional of {MinNotional} {Quote}",
-                    TypeName, symbol.Name, quantity, symbol.BaseAsset, ticker.ClosePrice, symbol.QuoteAsset, quantity * ticker.ClosePrice, symbol.QuoteAsset, symbol.Filters.MinNotional.MinNotional, symbol.QuoteAsset);
+                    TypeName, command.Symbol.Name, quantity, command.Symbol.BaseAsset, command.Ticker.ClosePrice, command.Symbol.QuoteAsset, quantity * command.Ticker.ClosePrice, command.Symbol.QuoteAsset, command.Symbol.Filters.MinNotional.MinNotional, command.Symbol.QuoteAsset);
 
                 return DesiredSell.None;
             }
 
             // otherwise we now have a valid desired sell
-            return new DesiredSell(quantity, ticker.ClosePrice);
+            return new DesiredSell(quantity, command.Ticker.ClosePrice);
         }
 
         private sealed record DesiredSell(decimal Quantity, decimal Price)

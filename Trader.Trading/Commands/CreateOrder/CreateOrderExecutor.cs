91301c26bbe851932993c77be9031b96161fb16d
jorge <jorge.candeias@protonmail.com>
2021-11-11 17:11:14 +0000

    Refactored

--- Trader.Trading/Commands/CreateOrder/CreateOrderExecutor.cs
+++ Trader.Trading/Commands/CreateOrder/CreateOrderExecutor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Outcompute.Trader.Models;
 using Outcompute.Trader.Trading.Algorithms;
 using Outcompute.Trader.Trading.Providers;
 using System.Diagnostics;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Outcompute.Trader.Trading.Commands.CreateOrder
 {
-    internal class CreateOrderExecutor : IAlgoCommandExecutor<CreateOrderCommand>
+    internal partial class CreateOrderExecutor : IAlgoCommandExecutor<CreateOrderCommand>
     {
         private readonly ILogger _logger;
         private readonly ITradingService _trader;
@@ -20,15 +21,13 @@ namespace Outcompute.Trader.Trading.Commands.CreateOrder
             _orders = orders;
         }
 
-        private static string TypeName => nameof(CreateOrderExecutor);
+        private static string TypeName { get; } = nameof(CreateOrderExecutor);
 
         public async Task ExecuteAsync(IAlgoContext context, CreateOrderCommand command, CancellationToken cancellationToken = default)
         {
             var watch = Stopwatch.StartNew();
 
-            _logger.LogInformation(
-                "{Type} {Name} placing {OrderType} {OrderSide} order for {Quantity:F8} {Asset} at {Price:F8} {Quote} for a total of {Total:F8} {Quote}",
-                TypeName, command.Symbol.Name, command.Type, command.Side, command.Quantity, command.Symbol.BaseAsset, command.Price, command.Symbol.QuoteAsset, command.Quantity * command.Price, command.Symbol.QuoteAsset);
+            LogPlacingOrder(TypeName, command.Symbol.Name, command.Type, command.Side, command.Quantity, command.Symbol.BaseAsset, command.Price, command.Symbol.QuoteAsset, command.Quantity * command.Price);
 
             var created = await _trader
                 .CreateOrderAsync(command.Symbol.Name, command.Side, command.Type, command.TimeInForce, command.Quantity, null, command.Price, command.Tag, null, null, cancellationToken)
@@ -38,9 +37,17 @@ namespace Outcompute.Trader.Trading.Commands.CreateOrder
                 .SetOrderAsync(created, 0m, 0m, 0m, cancellationToken)
                 .ConfigureAwait(false);
 
-            _logger.LogInformation(
-                "{Type} {Name} placed {OrderType} {OrderSide} order for {Quantity:F8} {Asset} at {Price:F8} {Quote} for a total of {Total:F8} {Quote} in {ElapsedMs}ms",
-                TypeName, command.Symbol.Name, command.Type, command.Side, command.Quantity, command.Symbol.BaseAsset, command.Price, command.Symbol.QuoteAsset, command.Quantity * command.Price, command.Symbol.QuoteAsset, watch.ElapsedMilliseconds);
+            LogPlacedOrder(TypeName, command.Symbol.Name, command.Type, command.Side, command.Quantity, command.Symbol.BaseAsset, command.Price, command.Symbol.QuoteAsset, command.Quantity * command.Price, watch.ElapsedMilliseconds);
         }
+
+        #region Logging
+
+        [LoggerMessage(0, LogLevel.Information, "{Type} {Name} placing {OrderType} {OrderSide} order for {Quantity:F8} {Asset} at {Price:F8} {Quote} for a total of {Total:F8} {Quote}")]
+        private partial void LogPlacingOrder(string type, string name, OrderType orderType, OrderSide orderSide, decimal quantity, string asset, decimal price, string quote, decimal total);
+
+        [LoggerMessage(0, LogLevel.Information, "{Type} {Name} placed {OrderType} {OrderSide} order for {Quantity:F8} {Asset} at {Price:F8} {Quote} for a total of {Total:F8} {Quote} in {ElapsedMs}ms")]
+        private partial void LogPlacedOrder(string type, string name, OrderType orderType, OrderSide orderSide, decimal quantity, string asset, decimal price, string quote, decimal total, long elapsedMs);
+
+        #endregion Logging
     }
 }

5848f51b2a51e04d3a2d39a292bee65a5e548ef0
jorge <jorge.candeias@protonmail.com>
2021-11-16 16:37:30 +0000

    Cleanup

--- Trader.Hosting.Abstractions/ITraderBuilder.cs
+++ Trader.Hosting.Abstractions/ITraderBuilder.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
-namespace Outcompute.Trader.Hosting
+namespace Outcompute.Trader.Hosting;
+
+public interface ITraderBuilder
 {
-    public interface ITraderBuilder
-    {
-        public ITraderBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configure);
+    public ITraderBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configure);
 
-        public ITraderBuilder ConfigureTrader(Action<HostBuilderContext, ITraderBuilder> configure);
-    }
+    public ITraderBuilder ConfigureTrader(Action<HostBuilderContext, ITraderBuilder> configure);
 }
--- Trader.Hosting.Abstractions/ITraderBuilderExtensions.cs
+++ Trader.Hosting.Abstractions/ITraderBuilderExtensions.cs
@@ -1,36 +1,34 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Orleans.Hosting;
-using System;
 
-namespace Outcompute.Trader.Hosting
+namespace Outcompute.Trader.Hosting;
+
+public static class ITraderBuilderExtensions
 {
-    public static class ITraderBuilderExtensions
+    public static ITraderBuilder ConfigureServices(this ITraderBuilder trader, Action<IServiceCollection> configure)
     {
-        public static ITraderBuilder ConfigureServices(this ITraderBuilder trader, Action<IServiceCollection> configure)
+        if (trader is null) throw new ArgumentNullException(nameof(trader));
+        if (configure is null) throw new ArgumentNullException(nameof(configure));
+
+        return trader.ConfigureServices((context, services) => configure(services));
+    }
+
+    public static ITraderBuilder ConfigureTrader(this ITraderBuilder trader, Action<ITraderBuilder> configure)
+    {
+        if (trader is null) throw new ArgumentNullException(nameof(trader));
+        if (configure is null) throw new ArgumentNullException(nameof(configure));
+
+        return trader.ConfigureTrader((context, trader) => configure(trader));
+    }
+
+    public static ITraderBuilder Configure<TOptions>(this ITraderBuilder trader, Action<TOptions> configure) where TOptions : class
+    {
+        if (trader is null) throw new ArgumentNullException(nameof(trader));
+        if (configure is null) throw new ArgumentNullException(nameof(configure));
+
+        return trader.ConfigureServices(services =>
         {
-            if (trader is null) throw new ArgumentNullException(nameof(trader));
-            if (configure is null) throw new ArgumentNullException(nameof(configure));
-
-            return trader.ConfigureServices((context, services) => configure(services));
-        }
-
-        public static ITraderBuilder ConfigureTrader(this ITraderBuilder trader, Action<ITraderBuilder> configure)
-        {
-            if (trader is null) throw new ArgumentNullException(nameof(trader));
-            if (configure is null) throw new ArgumentNullException(nameof(configure));
-
-            return trader.ConfigureTrader((context, trader) => configure(trader));
-        }
-
-        public static ITraderBuilder Configure<TOptions>(this ITraderBuilder trader, Action<TOptions> configure) where TOptions : class
-        {
-            if (trader is null) throw new ArgumentNullException(nameof(trader));
-            if (configure is null) throw new ArgumentNullException(nameof(configure));
-
-            return trader.ConfigureServices(services =>
-            {
-                services.Configure(configure);
-            });
-        }
+            services.Configure(configure);
+        });
     }
 }

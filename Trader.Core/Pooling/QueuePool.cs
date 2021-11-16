c897746b0fa9a9957c5aad6c870780618599e607
jorge <jorge.candeias@protonmail.com>
2021-11-16 16:29:21 +0000

    Cleanup

--- Trader.Core/Pooling/QueuePool.cs
+++ Trader.Core/Pooling/QueuePool.cs
@@ -1,28 +1,26 @@
 ﻿using Microsoft.Extensions.ObjectPool;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Outcompute.Trader.Core.Pooling
+namespace Outcompute.Trader.Core.Pooling;
+
+public static class QueuePool<T>
 {
-    public static class QueuePool<T>
+    [SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "Type Rooting Pattern")]
+    public static ObjectPool<Queue<T>> Shared { get; } = CorePoolProvider.Default.Create(QueuePooledObjectPolicy.Default);
+
+    private sealed class QueuePooledObjectPolicy : IPooledObjectPolicy<Queue<T>>
     {
-        [SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "Type Rooting Pattern")]
-        public static ObjectPool<Queue<T>> Shared { get; } = CorePoolProvider.Default.Create(QueuePooledObjectPolicy.Default);
-
-        private sealed class QueuePooledObjectPolicy : IPooledObjectPolicy<Queue<T>>
+        public Queue<T> Create()
         {
-            public Queue<T> Create()
-            {
-                return new Queue<T>();
-            }
-
-            public bool Return(Queue<T> obj)
-            {
-                obj.Clear();
-                return true;
-            }
-
-            public static QueuePooledObjectPolicy Default { get; } = new QueuePooledObjectPolicy();
+            return new Queue<T>();
         }
+
+        public bool Return(Queue<T> obj)
+        {
+            obj.Clear();
+            return true;
+        }
+
+        public static QueuePooledObjectPolicy Default { get; } = new QueuePooledObjectPolicy();
     }
 }

b9e2ae0098a6cc8acf459b89d5bd7f19b722a6bf
jorge <jorge.candeias@protonmail.com>
2021-11-16 16:37:02 +0000

    Cleanup

--- Trader.Hosting/TraderSiloBuilderExtensions.cs
+++ Trader.Hosting/TraderSiloBuilderExtensions.cs
@@ -1,23 +1,22 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
-namespace Orleans.Hosting
+namespace Orleans.Hosting;
+
+public static class TraderSiloBuilderExtensions
 {
-    public static class TraderSiloBuilderExtensions
+    public static ISiloBuilder AddTrader(this ISiloBuilder builder)
     {
-        public static ISiloBuilder AddTrader(this ISiloBuilder builder)
-        {
-            return builder
-                .AddTradingServices()
-                .ConfigureServices((context, services) =>
-                {
-                    services
-                        .AddSystemClock()
-                        .AddSafeTimerFactory()
-                        .AddBase62NumberSerializer()
-                        .AddModelAutoMapperProfiles()
-                        .AddRandomGenerator()
-                        .AddAlgoFactoryResolver();
-                });
-        }
+        return builder
+            .AddTradingServices()
+            .ConfigureServices((context, services) =>
+            {
+                services
+                    .AddSystemClock()
+                    .AddSafeTimerFactory()
+                    .AddBase62NumberSerializer()
+                    .AddModelAutoMapperProfiles()
+                    .AddRandomGenerator()
+                    .AddAlgoFactoryResolver();
+            });
     }
 }

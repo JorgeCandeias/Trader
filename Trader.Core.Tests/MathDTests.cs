f207b974a6dac8c58808d26d2118d34a11d24c71
jorge <jorge.candeias@protonmail.com>
2021-11-23 19:54:59 +0000

    Updated MathD

--- Trader.Core.Tests/MathDTests.cs
+++ Trader.Core.Tests/MathDTests.cs
@@ -5,6 +5,68 @@ namespace Outcompute.Trader.Core.Tests
     public class MathDTests
     {
         [Theory]
+        [InlineData(-0.1, 0.0)]
+        [InlineData(0.0, 0.0)]
+        [InlineData(0.1, 0.1)]
+        [InlineData(0.5, 0.5)]
+        [InlineData(0.9, 0.9)]
+        [InlineData(1.0, 1.0)]
+        [InlineData(1.1, 1.0)]
+        public void Clamps01(decimal value, decimal expected)
+        {
+            // act
+            var result = MathD.Clamp01(value);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        // low to high range
+        [InlineData(30, 70, -0.1, 30)]
+        [InlineData(30, 70, +0.0, 30)]
+        [InlineData(30, 70, +0.1, 34)]
+        [InlineData(30, 70, +0.2, 38)]
+        [InlineData(30, 70, +0.3, 42)]
+        [InlineData(30, 70, +0.4, 46)]
+        [InlineData(30, 70, +0.5, 50)]
+        [InlineData(30, 70, +0.6, 54)]
+        [InlineData(30, 70, +0.7, 58)]
+        [InlineData(30, 70, +0.8, 62)]
+        [InlineData(30, 70, +0.9, 66)]
+        [InlineData(30, 70, +1.0, 70)]
+        [InlineData(30, 70, +1.1, 70)]
+        // high to low range
+        [InlineData(70, 30, -0.1, 70)]
+        [InlineData(70, 30, +0.0, 70)]
+        [InlineData(70, 30, +0.1, 66)]
+        [InlineData(70, 30, +0.2, 62)]
+        [InlineData(70, 30, +0.3, 58)]
+        [InlineData(70, 30, +0.4, 54)]
+        [InlineData(70, 30, +0.5, 50)]
+        [InlineData(70, 30, +0.6, 46)]
+        [InlineData(70, 30, +0.7, 42)]
+        [InlineData(70, 30, +0.8, 38)]
+        [InlineData(70, 30, +0.9, 34)]
+        [InlineData(70, 30, +1.0, 30)]
+        [InlineData(70, 30, +1.1, 30)]
+        // zero range
+        [InlineData(50, 50, -0.5, 50)]
+        [InlineData(50, 50, +0.0, 50)]
+        [InlineData(50, 50, +0.5, 50)]
+        [InlineData(50, 50, +1.0, 50)]
+        [InlineData(50, 50, +1.5, 50)]
+        public void Lerps(decimal start, decimal end, decimal ratio, decimal expected)
+        {
+            // act
+            var result = MathD.Lerp(start, end, ratio);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        // low to high range
         [InlineData(30, 70, -0.1, 26)]
         [InlineData(30, 70, +0.0, 30)]
         [InlineData(30, 70, +0.1, 34)]
@@ -18,17 +80,79 @@ namespace Outcompute.Trader.Core.Tests
         [InlineData(30, 70, +0.9, 66)]
         [InlineData(30, 70, +1.0, 70)]
         [InlineData(30, 70, +1.1, 74)]
-        [InlineData(30, 30, +0.5, 30)]
-        public void Lerps(decimal start, decimal end, decimal ratio, decimal expected)
+        // high to low range
+        [InlineData(70, 30, -0.1, 74)]
+        [InlineData(70, 30, +0.0, 70)]
+        [InlineData(70, 30, +0.1, 66)]
+        [InlineData(70, 30, +0.2, 62)]
+        [InlineData(70, 30, +0.3, 58)]
+        [InlineData(70, 30, +0.4, 54)]
+        [InlineData(70, 30, +0.5, 50)]
+        [InlineData(70, 30, +0.6, 46)]
+        [InlineData(70, 30, +0.7, 42)]
+        [InlineData(70, 30, +0.8, 38)]
+        [InlineData(70, 30, +0.9, 34)]
+        [InlineData(70, 30, +1.0, 30)]
+        [InlineData(70, 30, +1.1, 26)]
+        // zero range
+        [InlineData(50, 50, -0.5, 50)]
+        [InlineData(50, 50, +0.0, 50)]
+        [InlineData(50, 50, +0.5, 50)]
+        [InlineData(50, 50, +1.0, 50)]
+        [InlineData(50, 50, +1.5, 50)]
+        public void LerpsUnclamped(decimal start, decimal end, decimal ratio, decimal expected)
         {
             // act
-            var result = MathD.Lerp(start, end, ratio);
+            var result = MathD.LerpUnclamped(start, end, ratio);
 
             // assert
             Assert.Equal(expected, result);
         }
 
         [Theory]
+        // low to high range
+        [InlineData(30, 70, 26, +0.0)]
+        [InlineData(30, 70, 30, +0.0)]
+        [InlineData(30, 70, 34, +0.1)]
+        [InlineData(30, 70, 38, +0.2)]
+        [InlineData(30, 70, 42, +0.3)]
+        [InlineData(30, 70, 46, +0.4)]
+        [InlineData(30, 70, 50, +0.5)]
+        [InlineData(30, 70, 54, +0.6)]
+        [InlineData(30, 70, 58, +0.7)]
+        [InlineData(30, 70, 62, +0.8)]
+        [InlineData(30, 70, 66, +0.9)]
+        [InlineData(30, 70, 70, +1.0)]
+        [InlineData(30, 70, 74, +1.0)]
+        // high to low range
+        [InlineData(70, 30, 74, +0.0)]
+        [InlineData(70, 30, 70, +0.0)]
+        [InlineData(70, 30, 66, +0.1)]
+        [InlineData(70, 30, 62, +0.2)]
+        [InlineData(70, 30, 58, +0.3)]
+        [InlineData(70, 30, 54, +0.4)]
+        [InlineData(70, 30, 50, +0.5)]
+        [InlineData(70, 30, 46, +0.6)]
+        [InlineData(70, 30, 42, +0.7)]
+        [InlineData(70, 30, 38, +0.8)]
+        [InlineData(70, 30, 34, +0.9)]
+        [InlineData(70, 30, 30, +1.0)]
+        [InlineData(70, 30, 26, +1.0)]
+        // zero range
+        [InlineData(50, 50, 45, +0.0)]
+        [InlineData(50, 50, 50, +0.0)]
+        [InlineData(50, 50, 55, +0.0)]
+        public void InverseLerps(decimal start, decimal end, decimal value, decimal expected)
+        {
+            // act
+            var result = MathD.InverseLerp(start, end, value);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        // low to high range
         [InlineData(30, 70, 26, -0.1)]
         [InlineData(30, 70, 30, +0.0)]
         [InlineData(30, 70, 34, +0.1)]
@@ -42,32 +166,113 @@ namespace Outcompute.Trader.Core.Tests
         [InlineData(30, 70, 66, +0.9)]
         [InlineData(30, 70, 70, +1.0)]
         [InlineData(30, 70, 74, +1.1)]
-        [InlineData(30, 30, 30, +0.0)]
-        [InlineData(30, 30, 25, +0.0)]
-        [InlineData(30, 30, 35, +0.0)]
-        public void InverseLerps(decimal start, decimal end, decimal value, decimal expected)
+        // high to low range
+        [InlineData(70, 30, 74, -0.1)]
+        [InlineData(70, 30, 70, +0.0)]
+        [InlineData(70, 30, 66, +0.1)]
+        [InlineData(70, 30, 62, +0.2)]
+        [InlineData(70, 30, 58, +0.3)]
+        [InlineData(70, 30, 54, +0.4)]
+        [InlineData(70, 30, 50, +0.5)]
+        [InlineData(70, 30, 46, +0.6)]
+        [InlineData(70, 30, 42, +0.7)]
+        [InlineData(70, 30, 38, +0.8)]
+        [InlineData(70, 30, 34, +0.9)]
+        [InlineData(70, 30, 30, +1.0)]
+        [InlineData(70, 30, 26, +1.1)]
+        // zero range
+        [InlineData(50, 50, 45, +0.0)]
+        [InlineData(50, 50, 50, +0.0)]
+        [InlineData(50, 50, 55, +0.0)]
+        public void InverseLerpsUnclamped(decimal start, decimal end, decimal value, decimal expected)
         {
             // act
-            var result = MathD.InverseLerp(start, end, value);
+            var result = MathD.InverseLerpUnclamped(start, end, value);
 
             // assert
             Assert.Equal(expected, result);
         }
 
         [Theory]
-        // 30-70 => 0-100
-        [InlineData(30, 70, 25, 0, 100, -12.5)]
-        [InlineData(30, 70, 30, 0, 100, 0.0)]
-        [InlineData(30, 70, 35, 0, 100, 12.5)]
-        [InlineData(30, 70, 40, 0, 100, 25.0)]
-        [InlineData(30, 70, 45, 0, 100, 37.5)]
-        [InlineData(30, 70, 50, 0, 100, 50.0)]
-        [InlineData(30, 70, 55, 0, 100, 62.5)]
-        [InlineData(30, 70, 60, 0, 100, 75.0)]
-        [InlineData(30, 70, 65, 0, 100, 87.5)]
-        [InlineData(30, 70, 70, 0, 100, 100.0)]
-        [InlineData(30, 70, 75, 0, 100, 112.5)]
-        // 30-70 => 10-110
+        // low-high => low-high
+        [InlineData(30, 70, 25, 10, 110, 10.0)]
+        [InlineData(30, 70, 30, 10, 110, 10.0)]
+        [InlineData(30, 70, 35, 10, 110, 22.5)]
+        [InlineData(30, 70, 40, 10, 110, 35.0)]
+        [InlineData(30, 70, 45, 10, 110, 47.5)]
+        [InlineData(30, 70, 50, 10, 110, 60.0)]
+        [InlineData(30, 70, 55, 10, 110, 72.5)]
+        [InlineData(30, 70, 60, 10, 110, 85.0)]
+        [InlineData(30, 70, 65, 10, 110, 97.5)]
+        [InlineData(30, 70, 70, 10, 110, 110.0)]
+        [InlineData(30, 70, 75, 10, 110, 110.0)]
+        // low-high => high-low
+        [InlineData(30, 70, 25, 110, 10, 110.0)]
+        [InlineData(30, 70, 30, 110, 10, 110.0)]
+        [InlineData(30, 70, 35, 110, 10, 97.5)]
+        [InlineData(30, 70, 40, 110, 10, 85.0)]
+        [InlineData(30, 70, 45, 110, 10, 72.5)]
+        [InlineData(30, 70, 50, 110, 10, 60.0)]
+        [InlineData(30, 70, 55, 110, 10, 47.5)]
+        [InlineData(30, 70, 60, 110, 10, 35.0)]
+        [InlineData(30, 70, 65, 110, 10, 22.5)]
+        [InlineData(30, 70, 70, 110, 10, 10.0)]
+        [InlineData(30, 70, 75, 110, 10, 10.0)]
+        // low-high => zero range
+        [InlineData(30, 70, 40, 10, 10, 10.0)]
+        [InlineData(30, 70, 50, 10, 10, 10.0)]
+        [InlineData(30, 70, 60, 10, 10, 10.0)]
+        // high-low => low-high
+        [InlineData(70, 30, 75, 10, 110, 10.0)]
+        [InlineData(70, 30, 70, 10, 110, 10.0)]
+        [InlineData(70, 30, 65, 10, 110, 22.5)]
+        [InlineData(70, 30, 60, 10, 110, 35.0)]
+        [InlineData(70, 30, 55, 10, 110, 47.5)]
+        [InlineData(70, 30, 50, 10, 110, 60.0)]
+        [InlineData(70, 30, 45, 10, 110, 72.5)]
+        [InlineData(70, 30, 40, 10, 110, 85.0)]
+        [InlineData(70, 30, 35, 10, 110, 97.5)]
+        [InlineData(70, 30, 30, 10, 110, 110.0)]
+        [InlineData(70, 30, 25, 10, 110, 110.0)]
+        // high-low => high-low
+        [InlineData(70, 30, 75, 110, 10, 110.0)]
+        [InlineData(70, 30, 70, 110, 10, 110.0)]
+        [InlineData(70, 30, 65, 110, 10, 97.5)]
+        [InlineData(70, 30, 60, 110, 10, 85.0)]
+        [InlineData(70, 30, 55, 110, 10, 72.5)]
+        [InlineData(70, 30, 50, 110, 10, 60.0)]
+        [InlineData(70, 30, 45, 110, 10, 47.5)]
+        [InlineData(70, 30, 40, 110, 10, 35.0)]
+        [InlineData(70, 30, 35, 110, 10, 22.5)]
+        [InlineData(70, 30, 30, 110, 10, 10.0)]
+        [InlineData(70, 30, 25, 110, 10, 10.0)]
+        // high-low => zero range
+        [InlineData(70, 30, 60, 10, 10, 10.0)]
+        [InlineData(70, 30, 50, 10, 10, 10.0)]
+        [InlineData(70, 30, 40, 10, 10, 10.0)]
+        // zero range => low-high
+        [InlineData(50, 50, 40, 10, 110, 10.0)]
+        [InlineData(50, 50, 50, 10, 110, 10.0)]
+        [InlineData(50, 50, 60, 10, 110, 10.0)]
+        // zero range => high-low
+        [InlineData(50, 50, 40, 110, 10, 110.0)]
+        [InlineData(50, 50, 50, 110, 10, 110.0)]
+        [InlineData(50, 50, 60, 110, 10, 110.0)]
+        // zero range => zero range
+        [InlineData(50, 50, 40, 110, 110, 110.0)]
+        [InlineData(50, 50, 50, 110, 110, 110.0)]
+        [InlineData(50, 50, 60, 110, 110, 110.0)]
+        public void LerpsBetween(decimal sourceStart, decimal sourceEnd, decimal sourceValue, decimal targetStart, decimal targetEnd, decimal expected)
+        {
+            // act
+            var result = MathD.LerpBetween(sourceStart, sourceEnd, sourceValue, targetStart, targetEnd);
+
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        // low-high => low-high
         [InlineData(30, 70, 25, 10, 110, -2.5)]
         [InlineData(30, 70, 30, 10, 110, 10.0)]
         [InlineData(30, 70, 35, 10, 110, 22.5)]
@@ -79,10 +284,66 @@ namespace Outcompute.Trader.Core.Tests
         [InlineData(30, 70, 65, 10, 110, 97.5)]
         [InlineData(30, 70, 70, 10, 110, 110.0)]
         [InlineData(30, 70, 75, 10, 110, 122.5)]
-        public void LerpsBetween(decimal sourceStart, decimal sourceEnd, decimal sourceValue, decimal targetStart, decimal targetEnd, decimal expected)
+        // low-high => high-low
+        [InlineData(30, 70, 25, 110, 10, 122.5)]
+        [InlineData(30, 70, 30, 110, 10, 110.0)]
+        [InlineData(30, 70, 35, 110, 10, 97.5)]
+        [InlineData(30, 70, 40, 110, 10, 85.0)]
+        [InlineData(30, 70, 45, 110, 10, 72.5)]
+        [InlineData(30, 70, 50, 110, 10, 60.0)]
+        [InlineData(30, 70, 55, 110, 10, 47.5)]
+        [InlineData(30, 70, 60, 110, 10, 35.0)]
+        [InlineData(30, 70, 65, 110, 10, 22.5)]
+        [InlineData(30, 70, 70, 110, 10, 10.0)]
+        [InlineData(30, 70, 75, 110, 10, -2.5)]
+        // low-high => zero range
+        [InlineData(30, 70, 40, 10, 10, 10.0)]
+        [InlineData(30, 70, 50, 10, 10, 10.0)]
+        [InlineData(30, 70, 60, 10, 10, 10.0)]
+        // high-low => low-high
+        [InlineData(70, 30, 75, 10, 110, -2.5)]
+        [InlineData(70, 30, 70, 10, 110, 10.0)]
+        [InlineData(70, 30, 65, 10, 110, 22.5)]
+        [InlineData(70, 30, 60, 10, 110, 35.0)]
+        [InlineData(70, 30, 55, 10, 110, 47.5)]
+        [InlineData(70, 30, 50, 10, 110, 60.0)]
+        [InlineData(70, 30, 45, 10, 110, 72.5)]
+        [InlineData(70, 30, 40, 10, 110, 85.0)]
+        [InlineData(70, 30, 35, 10, 110, 97.5)]
+        [InlineData(70, 30, 30, 10, 110, 110.0)]
+        [InlineData(70, 30, 25, 10, 110, 122.5)]
+        // high-low => high-low
+        [InlineData(70, 30, 75, 110, 10, 122.5)]
+        [InlineData(70, 30, 70, 110, 10, 110.0)]
+        [InlineData(70, 30, 65, 110, 10, 97.5)]
+        [InlineData(70, 30, 60, 110, 10, 85.0)]
+        [InlineData(70, 30, 55, 110, 10, 72.5)]
+        [InlineData(70, 30, 50, 110, 10, 60.0)]
+        [InlineData(70, 30, 45, 110, 10, 47.5)]
+        [InlineData(70, 30, 40, 110, 10, 35.0)]
+        [InlineData(70, 30, 35, 110, 10, 22.5)]
+        [InlineData(70, 30, 30, 110, 10, 10.0)]
+        [InlineData(70, 30, 25, 110, 10, -2.5)]
+        // high-low => zero range
+        [InlineData(70, 30, 60, 10, 10, 10.0)]
+        [InlineData(70, 30, 50, 10, 10, 10.0)]
+        [InlineData(70, 30, 40, 10, 10, 10.0)]
+        // zero range => low-high
+        [InlineData(50, 50, 40, 10, 110, 10.0)]
+        [InlineData(50, 50, 50, 10, 110, 10.0)]
+        [InlineData(50, 50, 60, 10, 110, 10.0)]
+        // zero range => high-low
+        [InlineData(50, 50, 40, 110, 10, 110.0)]
+        [InlineData(50, 50, 50, 110, 10, 110.0)]
+        [InlineData(50, 50, 60, 110, 10, 110.0)]
+        // zero range => zero range
+        [InlineData(50, 50, 40, 110, 110, 110.0)]
+        [InlineData(50, 50, 50, 110, 110, 110.0)]
+        [InlineData(50, 50, 60, 110, 110, 110.0)]
+        public void LerpsBetweenUnclamped(decimal sourceStart, decimal sourceEnd, decimal sourceValue, decimal targetStart, decimal targetEnd, decimal expected)
         {
             // act
-            var result = MathD.LerpBetween(sourceStart, sourceEnd, sourceValue, targetStart, targetEnd);
+            var result = MathD.LerpBetweenUnclamped(sourceStart, sourceEnd, sourceValue, targetStart, targetEnd);
 
             // assert
             Assert.Equal(expected, result);
--- Trader.Core/Mathematics/MathD.cs
+++ Trader.Core/Mathematics/MathD.cs
@@ -6,13 +6,37 @@
     public static class MathD
     {
         /// <summary>
-        /// Performs linear interpolation between the specified range.
+        /// Returns <paramref name="ratio"/> clamped to the inclusive range of [0.0, 1.0].
+        /// </summary>
+        /// <param name="ratio">The ratio to clamp.</param>
+        /// <returns>The clamped ratio.</returns>
+        public static decimal Clamp01(decimal ratio)
+        {
+            return Math.Clamp(ratio, 0M, 1M);
+        }
+
+        /// <summary>
+        /// Performs clamped linear interpolation between the specified range.
         /// </summary>
         /// <param name="start">The start of the range.</param>
         /// <param name="end">The end of the range.</param>
         /// <param name="ratio">A value between 0.0 and 1.0 to interpolate to range with.</param>
         /// <returns>The interpolated value from the range.</returns>
         public static decimal Lerp(decimal start, decimal end, decimal ratio)
+        {
+            ratio = Clamp01(ratio);
+
+            return LerpUnclamped(start, end, ratio);
+        }
+
+        /// <summary>
+        /// Performs unclamped linear interpolation between the specified range.
+        /// </summary>
+        /// <param name="start">The start of the range.</param>
+        /// <param name="end">The end of the range.</param>
+        /// <param name="ratio">A value between 0.0 and 1.0 to interpolate to range with.</param>
+        /// <returns>The interpolated value from the range.</returns>
+        public static decimal LerpUnclamped(decimal start, decimal end, decimal ratio)
         {
             var range = end - start;
             var adjusted = ratio * range;
@@ -22,13 +46,29 @@
         }
 
         /// <summary>
-        /// Calculate the relative linear interpolation ratio given the specified range and value.
+        /// Calculate the relative clamped linear interpolation ratio given the specified range and value.
         /// </summary>
         /// <param name="start">The start of the range.</param>
         /// <param name="end">The end of the range.</param>
         /// <param name="value">The value from the value.</param>
         /// <returns>The interpolation rate between 0.0 and 1.0.</returns>
         public static decimal InverseLerp(decimal start, decimal end, decimal value)
+        {
+            var rate = InverseLerpUnclamped(start, end, value);
+
+            rate = Clamp01(rate);
+
+            return rate;
+        }
+
+        /// <summary>
+        /// Calculate the relative unclamped linear interpolation ratio given the specified range and value.
+        /// </summary>
+        /// <param name="start">The start of the range.</param>
+        /// <param name="end">The end of the range.</param>
+        /// <param name="value">The value from the value.</param>
+        /// <returns>The interpolation rate between 0.0 and 1.0.</returns>
+        public static decimal InverseLerpUnclamped(decimal start, decimal end, decimal value)
         {
             if (start == end)
             {
@@ -43,7 +83,7 @@
         }
 
         /// <summary>
-        /// Converts a value from the source scale into a value of the target scale using linear interpolation.
+        /// Converts a value from the source scale into a value of the target scale using clamped linear interpolation.
         /// </summary>
         /// <param name="sourceStart">The start of the source range.</param>
         /// <param name="sourceEnd">The end of the source range.</param>
@@ -55,6 +95,23 @@
         {
             var ratio = InverseLerp(sourceStart, sourceEnd, sourceValue);
             var targetValue = Lerp(targetStart, targetEnd, ratio);
+
+            return targetValue;
+        }
+
+        /// <summary>
+        /// Converts a value from the source scale into a value of the target scale using unclamped linear interpolation.
+        /// </summary>
+        /// <param name="sourceStart">The start of the source range.</param>
+        /// <param name="sourceEnd">The end of the source range.</param>
+        /// <param name="sourceValue">The value from the source range to convert.</param>
+        /// <param name="targetStart">The start of the target range.</param>
+        /// <param name="targetEnd">The end of the target range.</param>
+        /// <returns>A value from target range.</returns>
+        public static decimal LerpBetweenUnclamped(decimal sourceStart, decimal sourceEnd, decimal sourceValue, decimal targetStart, decimal targetEnd)
+        {
+            var ratio = InverseLerpUnclamped(sourceStart, sourceEnd, sourceValue);
+            var targetValue = LerpUnclamped(targetStart, targetEnd, ratio);
 
             return targetValue;
         }

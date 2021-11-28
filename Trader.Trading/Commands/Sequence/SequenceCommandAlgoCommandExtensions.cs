a1bfdcd00daf08c017d663b284b5c38ed8b0a98d
jorge <jorge.candeias@protonmail.com>
2021-11-28 04:11:26 +0000

    Refactored

--- Trader.Trading/Commands/Sequence/SequenceCommandAlgoCommandExtensions.cs
+++ Trader.Trading/Commands/Sequence/SequenceCommandAlgoCommandExtensions.cs
@@ -9,6 +9,14 @@ namespace Outcompute.Trader.Trading.Commands
         /// </summary>
         public static IAlgoCommand Then(this IAlgoCommand before, IAlgoCommand after)
         {
+            // if the source command is already a sequence then reuse its commands
+            // this makes exception call stacks look tidier when one of them throws
+            if (before is SequenceCommand sequence)
+            {
+                return new SequenceCommand(sequence.Commands.Append(after));
+            }
+
+            // otherwise wrap them both in a sequence command
             return new SequenceCommand(before, after);
         }
     }

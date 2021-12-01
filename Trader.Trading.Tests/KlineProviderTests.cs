59dd657cf685b8f6791781ab8429b4fd3a5e4c38
jorge <jorge.candeias@protonmail.com>
2021-12-01 20:03:01 +0000

    Fixed test

--- Trader.Trading.Tests/KlineProviderTests.cs
+++ Trader.Trading.Tests/KlineProviderTests.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Moq;
+﻿using Moq;
 using Orleans;
 using Orleans.TestingHost;
-using Outcompute.Trader.Data;
 using Outcompute.Trader.Models;
 using Outcompute.Trader.Models.Collections;
-using Outcompute.Trader.Trading.Providers;
 using Outcompute.Trader.Trading.Providers.Klines;
 using Outcompute.Trader.Trading.Tests.Fixtures;
 using Xunit;
@@ -52,67 +49,78 @@ namespace Outcompute.Trader.Trading.Tests
         }
 
         [Fact]
-        public async Task SetKlineAsync()
+        public async Task SetsKline()
         {
             // arrange
-            var symbol = Guid.NewGuid().ToString();
+            var symbol = "ABCXYZ";
             var interval = KlineInterval.Days1;
             var openTime = DateTime.UtcNow.Date;
             var item = Kline.Empty with { Symbol = symbol, Interval = interval, OpenTime = openTime };
 
-            // act
-            await _cluster.ServiceProvider
-                .GetRequiredService<IKlineProvider>()
-                .SetKlineAsync(item);
+            var factory = Mock.Of<IGrainFactory>();
+            Mock.Get(factory)
+                .Setup(x => x.GetGrain<IKlineProviderReplicaGrain>($"{symbol}|{interval}", null).SetKlineAsync(item))
+                .Verifiable();
 
-            var result = await _cluster.ServiceProvider
-                .GetRequiredService<ITradingRepository>()
-                .GetKlinesAsync(symbol, interval, DateTime.MinValue, DateTime.MaxValue);
+            var provider = new KlineProvider(factory);
+
+            // act
+            await provider.SetKlineAsync(item);
 
             // assert
-            Assert.Collection(result, x => Assert.Equal(item, x));
+            Mock.Get(factory).VerifyAll();
         }
 
         [Fact]
-        public async Task TryGetKlineAsync()
+        public async Task GetsKline()
         {
             // arrange
-            var symbol = Guid.NewGuid().ToString();
+            var symbol = "ABCXYZ";
             var interval = KlineInterval.Days1;
             var openTime = DateTime.UtcNow.Date;
             var item = Kline.Empty with { Symbol = symbol, Interval = interval, OpenTime = openTime };
-            await _cluster.ServiceProvider.GetRequiredService<ITradingRepository>().SetKlineAsync(item);
+
+            var factory = Mock.Of<IGrainFactory>();
+            Mock.Get(factory)
+                .Setup(x => x.GetGrain<IKlineProviderReplicaGrain>($"{symbol}|{interval}", null).TryGetKlineAsync(openTime))
+                .ReturnsAsync(item)
+                .Verifiable();
+
+            var provider = new KlineProvider(factory);
 
             // act
-            var result = await _cluster.ServiceProvider
-                .GetRequiredService<IKlineProvider>()
-                .TryGetKlineAsync(symbol, interval, openTime)
-                .ConfigureAwait(false);
+            var result = await provider.TryGetKlineAsync(symbol, interval, openTime);
 
             // assert
-            Assert.Equal(item, result);
+            Assert.Same(item, result);
+            Mock.Get(factory).VerifyAll();
         }
 
         [Fact]
-        public async Task SetKlinesAsync()
+        public async Task SetsKlines()
         {
             // arrange
-            var symbol = Guid.NewGuid().ToString();
+            var symbol = "ABCXYZ";
             var interval = KlineInterval.Days1;
             var openTime = DateTime.UtcNow.Date;
-            var item = Kline.Empty with { Symbol = symbol, Interval = interval, OpenTime = openTime };
+            var items = new KlineCollection(new[]
+            {
+                Kline.Empty with { Symbol = symbol, Interval = interval, OpenTime = openTime },
+                Kline.Empty with { Symbol = symbol, Interval = interval, OpenTime = openTime.Subtract(TimeSpan.FromDays(1)) }
+            });
+
+            var factory = Mock.Of<IGrainFactory>();
+            Mock.Get(factory)
+                .Setup(x => x.GetGrain<IKlineProviderReplicaGrain>($"{symbol}|{interval}", null).SetKlinesAsync(items))
+                .Verifiable();
+
+            var provider = new KlineProvider(factory);
 
             // act
-            await _cluster.ServiceProvider
-                .GetRequiredService<IKlineProvider>()
-                .SetKlinesAsync(symbol, interval, new[] { item });
-
-            var result = await _cluster.ServiceProvider
-                .GetRequiredService<ITradingRepository>()
-                .GetKlinesAsync(symbol, interval, DateTime.MinValue, DateTime.MaxValue);
+            await provider.SetKlinesAsync(symbol, interval, items);
 
             // assert
-            Assert.Collection(result, x => Assert.Equal(item, x));
+            Mock.Get(factory).VerifyAll();
         }
 
         [Fact]
@@ -122,17 +130,21 @@ namespace Outcompute.Trader.Trading.Tests
             var symbol = Guid.NewGuid().ToString();
             var interval = KlineInterval.Days1;
             var openTime = DateTime.UtcNow.Date;
-            var item = Kline.Empty with { Symbol = symbol, Interval = interval, OpenTime = openTime };
-            await _cluster.ServiceProvider.GetRequiredService<ITradingRepository>().SetKlineAsync(item);
+
+            var factory = Mock.Of<IGrainFactory>();
+            Mock.Get(factory)
+                .Setup(x => x.GetGrain<IKlineProviderReplicaGrain>($"{symbol}|{interval}", null).TryGetLastOpenTimeAsync())
+                .ReturnsAsync(openTime)
+                .Verifiable();
+
+            var provider = new KlineProvider(factory);
 
             // act
-            var result = await _cluster.ServiceProvider
-                .GetRequiredService<IKlineProvider>()
-                .TryGetLastOpenTimeAsync(symbol, interval)
-                .ConfigureAwait(false);
+            var result = await provider.TryGetLastOpenTimeAsync(symbol, interval);
 
             // assert
             Assert.Equal(openTime, result);
+            Mock.Get(factory).VerifyAll();
         }
     }
 }

4bbefa8109f742d0f5f4e2d15240638617c6b64e
jorge <jorge.candeias@protonmail.com>
2021-12-02 02:50:40 +0000

    Fixed tests

--- Trader.Trading.Tests/TradeProviderTests.cs
+++ Trader.Trading.Tests/TradeProviderTests.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Moq;
+﻿using Moq;
 using Orleans;
 using Orleans.TestingHost;
 using Outcompute.Trader.Data;
 using Outcompute.Trader.Models;
-using Outcompute.Trader.Trading.Providers;
+using Outcompute.Trader.Models.Collections;
 using Outcompute.Trader.Trading.Providers.Trades;
 using Outcompute.Trader.Trading.Tests.Fixtures;
 using Xunit;
@@ -67,7 +66,7 @@ namespace Outcompute.Trader.Trading.Tests
         }
 
         [Fact]
-        public async Task SetsAndGetsTrades()
+        public async Task SetsTrades()
         {
             // arrange
             var symbol = Guid.NewGuid().ToString();
@@ -75,17 +74,49 @@ namespace Outcompute.Trader.Trading.Tests
             var trade2 = AccountTrade.Empty with { Symbol = symbol, Id = 2 };
             var trade3 = AccountTrade.Empty with { Symbol = symbol, Id = 3 };
             var trades = new[] { trade1, trade2, trade3 };
-            var provider = _cluster.ServiceProvider.GetRequiredService<ITradeProvider>();
+
+            var factory = Mock.Of<IGrainFactory>();
+            Mock.Get(factory)
+                .Setup(x => x.GetGrain<ITradeProviderReplicaGrain>(symbol, null).SetTradesAsync(trades))
+                .Verifiable();
+
+            var repository = Mock.Of<ITradingRepository>();
+
+            var provider = new TradeProvider(factory, repository);
 
             // act
             await provider.SetTradesAsync(symbol, trades);
-            var results = await provider.GetTradesAsync(symbol);
 
             // assert
-            Assert.Equal(3, results.Count);
-            Assert.Contains(trade1, results);
-            Assert.Contains(trade2, results);
-            Assert.Contains(trade3, results);
+            Mock.Get(factory).VerifyAll();
+        }
+
+        [Fact]
+        public async Task GetsTrades()
+        {
+            // arrange
+            var symbol = Guid.NewGuid().ToString();
+            var trade1 = AccountTrade.Empty with { Symbol = symbol, Id = 1 };
+            var trade2 = AccountTrade.Empty with { Symbol = symbol, Id = 2 };
+            var trade3 = AccountTrade.Empty with { Symbol = symbol, Id = 3 };
+            var trades = new TradeCollection(new[] { trade1, trade2, trade3 });
+
+            var factory = Mock.Of<IGrainFactory>();
+            Mock.Get(factory)
+                .Setup(x => x.GetGrain<ITradeProviderReplicaGrain>(symbol, null).GetTradesAsync())
+                .ReturnsAsync(trades)
+                .Verifiable();
+
+            var repository = Mock.Of<ITradingRepository>();
+
+            var provider = new TradeProvider(factory, repository);
+
+            // act
+            var result = await provider.GetTradesAsync(symbol);
+
+            // assert
+            Assert.Same(trades, result);
+            Mock.Get(factory).VerifyAll();
         }
     }
 }

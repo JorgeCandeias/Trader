ffdc70bc7b2db5dde76e4dc39a832231ff49454a
jorge <jorge.candeias@protonmail.com>
2021-12-02 02:35:07 +0000

    Fixed tests

--- Trader.Trading/Providers/Klines/KlineProviderReplicaGrainFactoryExtensions.cs
+++ Trader.Trading/Providers/Klines/KlineProviderReplicaGrainFactoryExtensions.cs
@@ -6,20 +6,22 @@ namespace Orleans;
 
 internal static class KlineProviderReplicaGrainFactoryExtensions
 {
-    private static readonly ConcurrentDictionary<(string Symbol, KlineInterval Interval), IKlineProviderReplicaGrain> _lookup = new();
+    private static readonly ConcurrentDictionary<(string Symbol, KlineInterval Interval), string> _lookup = new();
 
-    private static IKlineProviderReplicaGrain FactoryMethod((string Symbol, KlineInterval Interval) key, IGrainFactory factory)
+    private static string FactoryMethod((string Symbol, KlineInterval Interval) key)
     {
-        return factory.GetGrain<IKlineProviderReplicaGrain>($"{key.Symbol}|{key.Interval}");
+        return $"{key.Symbol}|{key.Interval}";
     }
 
-    private static readonly Func<(string Symbol, KlineInterval Interval), IGrainFactory, IKlineProviderReplicaGrain> FactoryDelegate = FactoryMethod;
+    private static readonly Func<(string Symbol, KlineInterval Interval), string> FactoryDelegate = FactoryMethod;
 
     public static IKlineProviderReplicaGrain GetKlineProviderReplicaGrain(this IGrainFactory factory, string symbol, KlineInterval interval)
     {
         if (factory is null) throw new ArgumentNullException(nameof(factory));
         if (symbol is null) throw new ArgumentNullException(nameof(symbol));
 
-        return _lookup.GetOrAdd((symbol, interval), FactoryDelegate, factory);
+        var key = _lookup.GetOrAdd((symbol, interval), FactoryDelegate);
+
+        return factory.GetGrain<IKlineProviderReplicaGrain>(key);
     }
 }

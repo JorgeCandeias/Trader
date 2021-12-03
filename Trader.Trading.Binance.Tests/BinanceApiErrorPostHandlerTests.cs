ca344774ae7c9611b85c7182b2c207305b3a7dca
jorge <jorge.candeias@protonmail.com>
2021-12-03 02:17:00 +0000

    Added tests

--- Trader.Trading.Binance.Tests/BinanceApiErrorPostHandlerTests.cs
+++ Trader.Trading.Binance.Tests/BinanceApiErrorPostHandlerTests.cs
@@ -5,6 +5,7 @@ using Moq;
 using Outcompute.Trader.Core.Time;
 using Outcompute.Trader.Trading.Binance.Handlers;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace Outcompute.Trader.Trading.Binance.Tests
 {
@@ -58,6 +59,117 @@ namespace Outcompute.Trader.Trading.Binance.Tests
 
             // assert
             Assert.Same(response, result);
+        }
+
+        [Fact]
+        public async Task HandlesTooManyRequestsWithDate()
+        {
+            // arrange
+            var options = Mock.Of<IOptions<BinanceOptions>>(x => x.Value.DefaultBackoffPeriod == TimeSpan.FromSeconds(10));
+            var logger = NullLogger<BinanceApiErrorPostHandler>.Instance;
+            var now = DateTime.UtcNow;
+            var clock = Mock.Of<ISystemClock>(x => x.UtcNow == now);
+            var retryAfter = now.AddSeconds(10);
+            using var handler = new BinanceApiErrorPostHandlerTester(options, logger, clock);
+            using var request = new HttpRequestMessage();
+            using var response = new HttpResponseMessage(HttpStatusCode.TooManyRequests);
+            response.Headers.RetryAfter = new RetryConditionHeaderValue(retryAfter);
+            handler.InnerHandler = new FakeMessageHandler(x =>
+            {
+                Assert.Same(request, x);
+                return response;
+            });
+
+            // act
+            var result = await Assert.ThrowsAsync<BinanceTooManyRequestsException>(async () => await handler.SendAsync(request, CancellationToken.None));
+
+            // assert
+            Assert.Equal(retryAfter.Subtract(now).Add(TimeSpan.FromSeconds(1)), result.RetryAfter);
+        }
+
+        [Fact]
+        public async Task HandlesTooManyRequestsWithDelta()
+        {
+            // arrange
+            var options = Mock.Of<IOptions<BinanceOptions>>(x => x.Value.DefaultBackoffPeriod == TimeSpan.FromSeconds(10));
+            var logger = NullLogger<BinanceApiErrorPostHandler>.Instance;
+            var now = DateTime.UtcNow;
+            var clock = Mock.Of<ISystemClock>(x => x.UtcNow == now);
+            var retryAfter = TimeSpan.FromSeconds(10);
+            using var handler = new BinanceApiErrorPostHandlerTester(options, logger, clock);
+            using var request = new HttpRequestMessage();
+            using var response = new HttpResponseMessage(HttpStatusCode.TooManyRequests);
+            response.Headers.RetryAfter = new RetryConditionHeaderValue(retryAfter);
+            handler.InnerHandler = new FakeMessageHandler(x =>
+            {
+                Assert.Same(request, x);
+                return response;
+            });
+
+            // act
+            var result = await Assert.ThrowsAsync<BinanceTooManyRequestsException>(async () => await handler.SendAsync(request, CancellationToken.None));
+
+            // assert
+            Assert.Equal(retryAfter.Add(TimeSpan.FromSeconds(1)), result.RetryAfter);
+        }
+
+        [Fact]
+        public async Task HandlesBinanceError()
+        {
+            // arrange
+            var options = Mock.Of<IOptions<BinanceOptions>>(x => x.Value.DefaultBackoffPeriod == TimeSpan.FromSeconds(123));
+            var logger = NullLogger<BinanceApiErrorPostHandler>.Instance;
+            var now = DateTime.UtcNow;
+            var clock = Mock.Of<ISystemClock>(x => x.UtcNow == now);
+            using var handler = new BinanceApiErrorPostHandlerTester(options, logger, clock);
+            using var request = new HttpRequestMessage();
+            using var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent(@"
+                {
+                    ""code"": 123,
+                    ""msg"": ""test""
+                }")
+            };
+            handler.InnerHandler = new FakeMessageHandler(x =>
+            {
+                Assert.Same(request, x);
+                return response;
+            });
+
+            // act
+            var result = await Assert.ThrowsAsync<BinanceCodeException>(async () => await handler.SendAsync(request, CancellationToken.None));
+
+            // assert
+            Assert.Equal(123, result.BinanceCode);
+            Assert.Equal("test", result.Message);
+        }
+
+        [Fact]
+        public async Task HandlesCommonError()
+        {
+            // arrange
+            var options = Mock.Of<IOptions<BinanceOptions>>(x => x.Value.DefaultBackoffPeriod == TimeSpan.FromSeconds(123));
+            var logger = NullLogger<BinanceApiErrorPostHandler>.Instance;
+            var now = DateTime.UtcNow;
+            var clock = Mock.Of<ISystemClock>(x => x.UtcNow == now);
+            using var handler = new BinanceApiErrorPostHandlerTester(options, logger, clock);
+            using var request = new HttpRequestMessage();
+            using var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent("{}")
+            };
+            handler.InnerHandler = new FakeMessageHandler(x =>
+            {
+                Assert.Same(request, x);
+                return response;
+            });
+
+            // act
+            var result = await Assert.ThrowsAsync<HttpRequestException>(async () => await handler.SendAsync(request, CancellationToken.None));
+
+            // assert
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
         }
     }
 }

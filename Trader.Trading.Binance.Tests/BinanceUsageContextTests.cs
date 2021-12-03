22d3b8e9d9a886b0f985a1759fa9ef1905127eec
jorge <jorge.candeias@protonmail.com>
2021-12-03 02:23:02 +0000

    Added test

---
+++ Trader.Trading.Binance.Tests/BinanceUsageContextTests.cs
@@ -0,0 +1,34 @@
+﻿using Outcompute.Trader.Models;
+
+namespace Outcompute.Trader.Trading.Binance.Tests;
+
+public class BinanceUsageContextTests
+{
+    [Fact]
+    public void SetsAndGets()
+    {
+        // arrange
+        var context = new BinanceUsageContext();
+        var type = RateLimitType.RequestWeight;
+        var window = TimeSpan.FromMinutes(1);
+        var limit = 100;
+        var used = 10;
+        var updated = DateTime.UtcNow;
+
+        // act
+        context.SetLimit(type, window, limit);
+        context.SetUsed(type, window, used, updated);
+        var result = context.EnumerateAll();
+
+        // assert
+        Assert.Collection(result,
+            x =>
+            {
+                Assert.Equal(type, x.Type);
+                Assert.Equal(window, x.Window);
+                Assert.Equal(used, x.Used);
+                Assert.Equal(updated, x.Updated);
+                Assert.Equal(limit, x.Limit);
+            });
+    }
+}

23b7fda53e648c1e62a6b22f70ef0e2fee540b37
jorge <jorge.candeias@protonmail.com>
2021-12-04 22:37:09 +0000

    Improved error handling on order creation

--- Trader.Trading/Commands/CreateOrder/CreateOrderExecutor.cs
+++ Trader.Trading/Commands/CreateOrder/CreateOrderExecutor.cs
@@ -23,6 +23,68 @@ internal partial class CreateOrderExecutor : IAlgoCommandExecutor<CreateOrderCom
 
     public async ValueTask ExecuteAsync(IAlgoContext context, CreateOrderCommand command, CancellationToken cancellationToken = default)
     {
+        var data = context.Data[command.Symbol.Name];
+        var ticker = data.Ticker.ClosePrice;
+
+        string asset;
+        decimal required;
+        decimal free;
+
+        switch (command.Side)
+        {
+            case OrderSide.Buy:
+
+                asset = command.Symbol.QuoteAsset;
+                free = data.Spot.QuoteAsset.Free;
+
+                if (command.Quantity.HasValue)
+                {
+                    required = command.Quantity.Value * (command.Price ?? ticker);
+                }
+                else if (command.Notional.HasValue)
+                {
+                    required = command.Notional.Value;
+                }
+                else
+                {
+                    ThrowHelper.ThrowInvalidOperationException();
+                    return;
+                }
+
+                break;
+
+            case OrderSide.Sell:
+
+                asset = command.Symbol.BaseAsset;
+                free = data.Spot.BaseAsset.Free;
+
+                if (command.Quantity.HasValue)
+                {
+                    required = command.Quantity.Value;
+                }
+                else if (command.Notional.HasValue)
+                {
+                    required = command.Notional.Value / (command.Price ?? ticker);
+                }
+                else
+                {
+                    ThrowHelper.ThrowInvalidOperationException();
+                    return;
+                }
+
+                break;
+
+            default:
+                ThrowHelper.ThrowInvalidOperationException();
+                return;
+        }
+
+        if (free < required)
+        {
+            LogCannotPlaceOrderWithoutFreeQuantity(TypeName, context.Name, command.Type, command.Side, required, asset, free);
+            return;
+        }
+
         var watch = Stopwatch.StartNew();
 
         LogPlacingOrder(TypeName, command.Symbol.Name, command.Type, command.Side, command.Quantity, command.Notional, command.Symbol.BaseAsset, command.Price, command.Symbol.QuoteAsset, command.Notional.HasValue ? command.Notional : command.Quantity * command.Price);
@@ -40,11 +102,14 @@ internal partial class CreateOrderExecutor : IAlgoCommandExecutor<CreateOrderCom
 
     #region Logging
 
-    [LoggerMessage(0, LogLevel.Information, "{Type} {Name} placing {OrderType} {OrderSide} order with (Quantity: {Quantity:F8} {Asset}, Notional: {Notional:F8} {Quote}, Price: {Price:F8} {Quote}, Total: {Total:F8} {Quote})")]
+    [LoggerMessage(1, LogLevel.Information, "{Type} {Name} placing {OrderType} {OrderSide} order with (Quantity: {Quantity:F8} {Asset}, Notional: {Notional:F8} {Quote}, Price: {Price:F8} {Quote}, Total: {Total:F8} {Quote})")]
     private partial void LogPlacingOrder(string type, string name, OrderType orderType, OrderSide orderSide, decimal? quantity, decimal? notional, string asset, decimal? price, string quote, decimal? total);
 
-    [LoggerMessage(1, LogLevel.Information, "{Type} {Name} placed {OrderType} {OrderSide} order with (Quantity: {Quantity:F8} {Asset}, Notional: {Notional:F8} {Quote}, Price: {Price:F8} {Quote}, Total: {Total:F8} {Quote}) in {ElapsedMs}ms")]
+    [LoggerMessage(2, LogLevel.Information, "{Type} {Name} placed {OrderType} {OrderSide} order with (Quantity: {Quantity:F8} {Asset}, Notional: {Notional:F8} {Quote}, Price: {Price:F8} {Quote}, Total: {Total:F8} {Quote}) in {ElapsedMs}ms")]
     private partial void LogPlacedOrder(string type, string name, OrderType orderType, OrderSide orderSide, decimal? quantity, decimal? notional, string asset, decimal? price, string quote, decimal? total, long elapsedMs);
+
+    [LoggerMessage(3, LogLevel.Error, "{Type} {Name} cannot place {OrderType} {OrderSide} order requiring {Quantity:F8} {Asset} because there is only {Free:F8} {Asset} free")]
+    private partial void LogCannotPlaceOrderWithoutFreeQuantity(string type, string name, OrderType orderType, OrderSide orderSide, decimal quantity, string asset, decimal free);
 
     #endregion Logging
 }

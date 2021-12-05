f8e18083375bdece1d6b96a1f28008e141c9268a
jorge <jorge.candeias@protonmail.com>
2021-12-05 14:08:44 +0000

    Removed unused fixture

--- Trader.Trading.Binance.Tests/Fixtures/ClusterCollectionFixture.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xunit;
-
-namespace Outcompute.Trader.Trading.Binance.Tests.Fixtures
-{
-    [CollectionDefinition(nameof(ClusterCollectionFixture))]
-    public class ClusterCollectionFixture : ICollectionFixture<ClusterFixture>
-    {
-    }
-}

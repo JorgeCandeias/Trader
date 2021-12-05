05dc05d156a51ae7de05af8a77ba9111d538349e
jorge <jorge.candeias@protonmail.com>
2021-12-05 20:21:29 +0000

    Added test

---
+++ Trader.Trading.Tests/AlgoContextExchangeInfoConfiguratorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Outcompute.Trader.Models;
+using Outcompute.Trader.Trading.Algorithms.Context;
+using Outcompute.Trader.Trading.Algorithms.Context.Configurators;
+using Outcompute.Trader.Trading.Providers;
+
+namespace Outcompute.Trader.Trading.Tests;
+
+public class AlgoContextExchangeInfoConfiguratorTests
+{
+    [Fact]
+    public async Task Configures()
+    {
+        // arrange
+        var name = "Algo1";
+
+        var info = ExchangeInfo.Empty with { ServerTime = DateTime.UtcNow };
+
+        var exchange = Mock.Of<IExchangeInfoProvider>(x => x.GetExchangeInfo() == info);
+
+        var configurator = new AlgoContextExchangeInfoConfigurator(exchange);
+
+        var provider = new ServiceCollection()
+            .BuildServiceProvider();
+
+        var context = new AlgoContext(name, provider);
+
+        // act
+        await configurator.ConfigureAsync(context, name);
+
+        // assert
+        Assert.Same(info, context.Exchange);
+    }
+}

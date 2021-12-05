85431670970ffd7fabdf1665e26994325c157d3c
jorge <jorge.candeias@protonmail.com>
2021-12-05 23:09:53 +0000

    Cleanup

--- Trader.Trading.Tests/AlgoOptionsConfiguratorTests.cs
+++ Trader.Trading.Tests/AlgoOptionsConfiguratorTests.cs
@@ -2,39 +2,36 @@
 using Microsoft.Extensions.Options;
 using Outcompute.Trader.Trading.Algorithms;
 using Outcompute.Trader.Trading.Configuration;
-using System.Collections.Generic;
-using Xunit;
 
-namespace Outcompute.Trader.Trading.Tests
+namespace Outcompute.Trader.Trading.Tests;
+
+public class AlgoOptionsConfiguratorTests
 {
-    public class AlgoOptionsConfiguratorTests
+    private class TestOptions
     {
-        private class TestOptions
-        {
-            public string MyValue { get; set; } = string.Empty;
-        }
+        public string MyValue { get; set; } = string.Empty;
+    }
 
-        [Fact]
-        public void ConfiguresNamedOptions()
-        {
-            // arrange
-            var name = "TestName";
-            var value = "TestValue";
-            var mapping = new AlgoConfigurationMappingOptions();
-            var config = new ConfigurationBuilder()
-                .AddInMemoryCollection(new Dictionary<string, string>
-                {
+    [Fact]
+    public void ConfiguresNamedOptions()
+    {
+        // arrange
+        var name = "TestName";
+        var value = "TestValue";
+        var mapping = new AlgoConfigurationMappingOptions();
+        var config = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
                     { $"{mapping.AlgosKey}:{name}:{mapping.AlgoOptionsSubKey}:MyValue", value }
-                })
-                .Build();
-            var configurator = new AlgoUserOptionsConfigurator<TestOptions>(Options.Create(mapping), config);
-            var options = new TestOptions();
+            })
+            .Build();
+        var configurator = new AlgoUserOptionsConfigurator<TestOptions>(Options.Create(mapping), config);
+        var options = new TestOptions();
 
-            // act
-            configurator.Configure(name, options);
+        // act
+        configurator.Configure(name, options);
 
-            // assert
-            Assert.Equal(value, options.MyValue);
-        }
+        // assert
+        Assert.Equal(value, options.MyValue);
     }
 }

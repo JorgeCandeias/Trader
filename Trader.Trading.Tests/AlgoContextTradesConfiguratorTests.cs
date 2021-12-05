d05524e3ca5295259748d7a4c57478941081705c
jorge <jorge.candeias@protonmail.com>
2021-12-05 22:28:04 +0000

    Added test

---
+++ Trader.Trading.Tests/AlgoContextTradesConfiguratorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Outcompute.Trader.Models;
+using Outcompute.Trader.Models.Collections;
+using Outcompute.Trader.Trading.Algorithms.Context;
+using Outcompute.Trader.Trading.Algorithms.Context.Configurators;
+using Outcompute.Trader.Trading.Providers;
+
+namespace Outcompute.Trader.Trading.Tests;
+
+public class AlgoContextTradesConfiguratorTests
+{
+    [Fact]
+    public async Task Configures()
+    {
+        // arrange
+        var name = "Algo1";
+
+        var symbol = Symbol.Empty with
+        {
+            Name = "ABCXYZ"
+        };
+
+        var trades = new TradeCollection(Array.Empty<AccountTrade>());
+
+        var tradeProvider = Mock.Of<ITradeProvider>();
+        Mock.Get(tradeProvider)
+            .Setup(x => x.GetTradesAsync(symbol.Name, CancellationToken.None))
+            .ReturnsAsync(trades)
+            .Verifiable();
+
+        var configurator = new AlgoContextTradesConfigurator(tradeProvider);
+
+        var provider = new ServiceCollection()
+            .BuildServiceProvider();
+
+        var context = new AlgoContext(name, provider);
+        context.Symbols.AddOrUpdate(symbol);
+
+        // act
+        await configurator.ConfigureAsync(context, name);
+
+        // assert
+        Assert.Same(trades, context.Data[symbol.Name].Trades);
+        Mock.Get(tradeProvider).VerifyAll();
+    }
+}

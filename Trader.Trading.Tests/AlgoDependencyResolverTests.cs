9e38fbd5279ca8696d8a38662bf1fb3646e24a21
jorge <jorge.candeias@protonmail.com>
2021-12-05 22:55:55 +0000

    Added test

---
+++ Trader.Trading.Tests/AlgoDependencyResolverTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using Outcompute.Trader.Trading.Algorithms;
+
+namespace Outcompute.Trader.Trading.Tests;
+
+public class AlgoDependencyResolverTests
+{
+    [Fact]
+    public void ExposesOptions()
+    {
+        // arrange
+        var options = new AlgoDependencyOptions();
+        var monitor = Mock.Of<IOptionsMonitor<AlgoDependencyOptions>>(x => x.CurrentValue == options);
+        var resolver = new AlgoDependencyResolver(monitor);
+
+        // act
+        var symbols = resolver.Symbols;
+        var klines = resolver.Klines;
+
+        // assert
+        Assert.Same(options.Symbols, symbols);
+        Assert.Same(options.Klines, klines);
+    }
+}

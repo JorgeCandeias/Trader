871f519c04f3c0223a90e64bab8f314b8471a5b4
jorge <jorge.candeias@protonmail.com>
2021-12-05 21:43:20 +0000

    Added test

---
+++ Trader.Trading.Tests/AlgoContextTickTimeConfiguratorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Outcompute.Trader.Core.Time;
+using Outcompute.Trader.Trading.Algorithms.Context;
+using Outcompute.Trader.Trading.Algorithms.Context.Configurators;
+
+namespace Outcompute.Trader.Trading.Tests;
+
+public class AlgoContextTickTimeConfiguratorTests
+{
+    [Fact]
+    public async Task Configures()
+    {
+        // arrange
+        var name = "Algo1";
+
+        var time = DateTime.UtcNow;
+
+        var clock = Mock.Of<ISystemClock>(x => x.UtcNow == time);
+
+        var configurator = new AlgoContextTickTimeConfigurator(clock);
+
+        var provider = new ServiceCollection()
+            .BuildServiceProvider();
+
+        var context = new AlgoContext(name, provider);
+
+        // act
+        await configurator.ConfigureAsync(context, name);
+
+        // assert
+        Assert.Equal(time, context.TickTime);
+        Mock.Get(clock).VerifyAll();
+    }
+}

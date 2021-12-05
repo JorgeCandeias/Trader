6e6b4bd8af4252194ba0af363bd9b6c350d6af6e
jorge <jorge.candeias@protonmail.com>
2021-12-05 20:35:07 +0000

    Added test

---
+++ Trader.Trading.Tests/AlgoContextFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Outcompute.Trader.Trading.Algorithms.Context;
+
+namespace Outcompute.Trader.Trading.Tests;
+
+public class AlgoContextFactoryTests
+{
+    [Fact]
+    public void CreatesContext()
+    {
+        // arrange
+        var name = "Algo1";
+
+        var local = new AlgoContextLocal();
+
+        var provider = new ServiceCollection()
+            .AddScoped<IAlgoContextLocal>(_ => local)
+            .BuildServiceProvider();
+
+        var factory = new AlgoContextFactory(provider);
+
+        // act
+        var result = factory.Create(name);
+
+        // assert
+        Assert.NotNull(result);
+        Assert.Equal(name, result.Name);
+        Assert.Same(provider, result.ServiceProvider);
+        Assert.Same(result, local.Context);
+    }
+}

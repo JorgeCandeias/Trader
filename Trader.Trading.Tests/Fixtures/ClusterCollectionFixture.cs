e0fb71f4fa196cabe456ae0b3d051bde00d87055
jorge <jorge.candeias@protonmail.com>
2021-12-05 18:59:57 +0000

    Removed unused test code

--- Trader.Trading.Tests/Fixtures/ClusterCollectionFixture.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xunit;
-
-namespace Outcompute.Trader.Trading.Tests.Fixtures
-{
-    [CollectionDefinition(nameof(ClusterCollectionFixture))]
-    public class ClusterCollectionFixture : ICollectionFixture<ClusterFixture>
-    {
-    }
-}

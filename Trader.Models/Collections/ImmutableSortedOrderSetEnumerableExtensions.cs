5fca81318530b5f226acc43c2f7e2f44002f7db2
jorge <jorge.candeias@protonmail.com>
2021-12-06 13:47:27 +0000

    Refactored

--- Trader.Models/Collections/ImmutableSortedOrderSetEnumerableExtensions.cs
+++ Trader.Models/Collections/ImmutableSortedOrderSetEnumerableExtensions.cs
@@ -5,6 +5,9 @@ namespace System.Collections.Generic
 {
     public static class ImmutableSortedOrderSetEnumerableExtensions
     {
-        public static ImmutableSortedOrderSet ToImmutableSortedOrderSet(this IEnumerable<OrderQueryResult> items) => ImmutableSortedOrderSet.Create(items);
+        public static ImmutableSortedOrderSet ToImmutableSortedOrderSet(this IEnumerable<OrderQueryResult> items)
+        {
+            return ImmutableSortedOrderSet.Create(items);
+        }
     }
 }

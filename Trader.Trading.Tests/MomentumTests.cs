f4097204a930d019177bb29f08dceaf55cf0f2fc
jorge <jorge.candeias@protonmail.com>
2021-12-07 00:48:22 +0000

    Added basic Momentum Indicator

---
+++ Trader.Trading.Tests/MomentumTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Toolkit.Diagnostics;
+
+namespace Outcompute.Trader.Trading.Tests;
+
+public class MomentumTests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Evaluates(decimal[] source, decimal[] expected, int periods)
+    {
+        Guard.IsNotNull(source, nameof(source));
+        Guard.IsNotNull(expected, nameof(expected));
+
+        var momentum = source.Momentum(periods).ToArray();
+
+        for (var i = 0; i < expected.Length; i++)
+        {
+            Assert.Equal(expected[i], momentum[i]);
+        }
+    }
+
+    public static readonly IEnumerable<object[]> TestData = new object[][]{
+        new object[] { new decimal[] { 1, 1, 1, 1, 1, 1 }, new decimal[] { 0, 0, 0, 0, 0, 0 }, 1 },
+        new object[] { new decimal[] { 1, 2, 3, 4, 5, 6 }, new decimal[] { 0, 1, 1, 1, 1, 1 }, 1 },
+        new object[] { new decimal[] { 1, 2, 3, 5, 8, 13 }, new decimal[] { 0, 1, 1, 2, 3, 5 }, 1 },
+
+        new object[] { new decimal[] { 1, 3, 2, 13, 8, 5 }, new decimal[] { 0, 2, -1, 11, -5, -3 }, 1 },
+
+        new object[] { new decimal[] { 1, 1, 1, 1, 1, 1 }, new decimal[] { 0, 0, 0, 0, 0, 0 }, 2 },
+        new object[] { new decimal[] { 1, 2, 3, 4, 5, 6 }, new decimal[] { 0, 0, 2, 2, 2, 2 }, 2 },
+        new object[] { new decimal[] { 1, 2, 3, 5, 8, 13 }, new decimal[] { 0, 0, 2, 3, 5, 8 }, 2 },
+
+        new object[] { new decimal[] { 1, 3, 2, 13, 8, 5 }, new decimal[] { 0, 0, 1, 10, 6, -8 }, 2 },
+
+        new object[] { new decimal[] { 1, 1, 1, 1, 1, 1 }, new decimal[] { 0, 0, 0, 0, 0, 0 }, 3 },
+        new object[] { new decimal[] { 1, 2, 3, 4, 5, 6 }, new decimal[] { 0, 0, 0, 3, 3, 3 }, 3 },
+        new object[] { new decimal[] { 1, 2, 3, 5, 8, 13 }, new decimal[] { 0, 0, 0, 4, 6, 10 }, 3 },
+
+        new object[] { new decimal[] { 1, 3, 2, 13, 8, 5 }, new decimal[] { 0, 0, 0, 12, 5, 3 }, 3 },
+    };
+}
---
+++ Trader.Trading/Indicators/MomentumExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Outcompute.Trader.Core.Pooling;
+
+namespace System.Collections.Generic;
+
+internal static class MomentumExtensions
+{
+    public static IEnumerable<decimal> Momentum(this IEnumerable<decimal> source, int periods)
+    {
+        Guard.IsNotNull(source, nameof(source));
+
+        var enumerator = source.GetEnumerator();
+        var queue = QueuePool<decimal>.Shared.Get();
+
+        // advance the first n periods
+        for (var i = 0; i < periods; i++)
+        {
+            if (enumerator.MoveNext())
+            {
+                queue.Enqueue(enumerator.Current);
+                yield return 0;
+            }
+            else
+            {
+                QueuePool<decimal>.Shared.Return(queue);
+                yield break;
+            }
+        }
+
+        // return differences for the rest of the enumeration
+        while (enumerator.MoveNext())
+        {
+            var current = enumerator.Current;
+
+            var comparand = queue.Dequeue();
+            queue.Enqueue(current);
+
+            yield return current - comparand;
+        }
+
+        QueuePool<decimal>.Shared.Return(queue);
+    }
+
+    public static IEnumerable<decimal> Momentum<T>(this IEnumerable<T> source, Func<T, decimal> selector, int periods)
+    {
+        Guard.IsNotNull(source, nameof(source));
+        Guard.IsNotNull(selector, nameof(selector));
+
+        return source.Select(selector).Momentum(periods);
+    }
+}

2aad5a7afcf4f08c6b471044b0bb0ec513245afe
jorge <jorge.candeias@protonmail.com>
2022-03-15 17:01:42 +0000

    Added WmaIndicator

---
+++ Trader.Trading.Tests/WmaIndicatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Outcompute.Trader.Core.Mathematics;
+using Outcompute.Trader.Trading.Indicators;
+
+namespace Outcompute.Trader.Trading.Tests;
+
+public class WmaIndicatorTests
+{
+    [Fact]
+    public void YieldsWma()
+    {
+        // act
+        var indicator = new WmaIndicator(3)
+        {
+            10254.92M, 10171.06M, 9998.87M, 10010.53M, 9706.93M, 8499.75M, 8436.75M, 8067.78M, 8187.15M, 8206.39M
+        };
+
+        // assert
+        Assert.Collection(indicator,
+            x => Assert.Null(x),
+            x => Assert.Null(x),
+            x => Assert.Equal(10098.94M, MathN.Round(x, 2)),
+            x => Assert.Equal(10033.40M, MathN.Round(x, 2)),
+            x => Assert.Equal(9856.79M, MathN.Round(x, 2)),
+            x => Assert.Equal(9153.94M, MathN.Round(x, 2)),
+            x => Assert.Equal(8669.45M, MathN.Round(x, 2)),
+            x => Assert.Equal(8262.76M, MathN.Round(x, 2)),
+            x => Assert.Equal(8188.96M, MathN.Round(x, 2)),
+            x => Assert.Equal(8176.88M, MathN.Round(x, 2)));
+    }
+}

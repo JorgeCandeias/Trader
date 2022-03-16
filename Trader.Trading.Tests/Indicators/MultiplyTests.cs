1299084e3769a5fd6e9488d9f731b75d86f55603
jorge <jorge.candeias@protonmail.com>
2022-03-16 13:29:30 +0000

    Added Multiply Indicator tests

---
+++ Trader.Trading.Tests/Indicators/MultiplyTests.cs
@@ -0,0 +1,59 @@
+﻿using Outcompute.Trader.Trading.Indicators;
+
+namespace Outcompute.Trader.Trading.Tests.Indicators
+{
+    public static class MultiplyTests
+    {
+        [Fact]
+        public static void YieldsOutput()
+        {
+            // arrange
+            using var first = new Identity<decimal?> { 4, 6, 8, null, 1 };
+            using var second = new Identity<decimal?> { 2, 2, 2, 1, null };
+
+            // act
+            using var indicator = new Multiply(first, second);
+
+            // assert
+            Assert.Collection(indicator,
+                x => Assert.Equal(8, x),
+                x => Assert.Equal(12, x),
+                x => Assert.Equal(16, x),
+                x => Assert.Null(x),
+                x => Assert.Null(x));
+        }
+
+        [Fact]
+        public static void ThrowsOnInPlaceUpdate()
+        {
+            // arrange
+            using var first = new Identity<decimal?> { 4, 6, 8 };
+            using var second = new Identity<decimal?> { 2, 2, 2 };
+            using var indicator = new Multiply(first, second);
+
+            // assert
+            Assert.Throws<NotSupportedException>(() => indicator.Update(2, -4));
+        }
+
+        [Fact]
+        public static void UpdatesFromSource()
+        {
+            // arrange
+            using var first = new Identity<decimal?> { 4, 6, 8, null, 1 };
+            using var second = new Identity<decimal?> { 2, 2, 2, 1, null };
+            using var indicator = new Multiply(first, second);
+
+            // act
+            first.Update(1, 8);
+            second.Update(2, 4);
+
+            // assert
+            Assert.Collection(indicator,
+                x => Assert.Equal(8, x),
+                x => Assert.Equal(16, x),
+                x => Assert.Equal(32, x),
+                x => Assert.Null(x),
+                x => Assert.Null(x));
+        }
+    }
+}

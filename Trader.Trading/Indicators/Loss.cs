241cff2da15ad8e1421f77e33a1c574d53ee701e
jorge <jorge.candeias@protonmail.com>
2022-03-17 21:40:54 +0000

    Refactored Loss Indicator

---
+++ Trader.Trading/Indicators/Loss.cs
@@ -0,0 +1,60 @@
+﻿using Outcompute.Trader.Core.Mathematics;
+
+namespace Outcompute.Trader.Trading.Indicators;
+
+/// <summary>
+/// Calculates the loss between the current value and the previous value.
+/// </summary>
+public class Loss : IndicatorBase<decimal?, decimal?>
+{
+    public Loss()
+    {
+    }
+
+    public Loss(IIndicatorResult<decimal?> source) : this()
+    {
+        Guard.IsNotNull(source, nameof(source));
+
+        LinkFrom(source);
+    }
+
+    protected override decimal? Calculate(int index)
+    {
+        if (index < 1)
+        {
+            return null;
+        }
+
+        return MathN.Min(Source[index] - Source[index - 1], 0, MinMaxBehavior.NullWins);
+    }
+}
+
+public static partial class Indicator
+{
+    public static Loss Loss() => new();
+
+    public static Loss Loss(IIndicatorResult<decimal?> source) => new(source);
+}
+
+public static class LossEnumerableExtensions
+{
+    public static IEnumerable<decimal?> Loss(this IEnumerable<decimal?> source)
+    {
+        return source.Loss(x => x);
+    }
+
+    public static IEnumerable<decimal?> Loss<T>(this IEnumerable<T> source, Func<T, decimal?> selector)
+    {
+        Guard.IsNotNull(source, nameof(source));
+        Guard.IsNotNull(selector, nameof(selector));
+
+        using var indicator = Indicator.Loss();
+
+        foreach (var item in source)
+        {
+            indicator.Add(selector(item));
+
+            yield return indicator[^1];
+        }
+    }
+}
